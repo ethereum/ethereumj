b7d827a87a30652c75a419280bd86c212cdfdecd
nicksavers <nicksavers@gmail.com>
2014-05-05 12:55:25 +0200

    Add sign function to Transaction

--- ethereumj-core/src/main/java/org/ethereum/net/message/TransactionsMessage.java
+++ ethereumj-core/src/main/java/org/ethereum/net/message/TransactionsMessage.java
@@ -29,7 +29,7 @@ public class TransactionsMessage extends Message {
     public void parseRLP() {
         RLPList paramsList = (RLPList) rawData.getElement(0);
 
-        if ( Command.fromInt(((RLPItem)(paramsList).getElement(0)).getData()[0] & 0xFF) != TRANSACTIONS){
+        if (Command.fromInt(((RLPItem)(paramsList).getElement(0)).getData()[0] & 0xFF) != TRANSACTIONS) {
             throw new Error("TransactionMessage: parsing for mal data");
         }
 
@@ -53,10 +53,10 @@ public class TransactionsMessage extends Message {
         return null;
     }
 
-    public String toString(){
+    public String toString() {
         if(!parsed) parseRLP();
         StringBuffer sb = new StringBuffer();
-        for (Transaction transactionData : transactions){
+        for (Transaction transactionData : transactions) {
             sb.append("   ").append(transactionData).append("\n");
         }
         return "Transactions Message [\n" + sb.toString() + " ]";
