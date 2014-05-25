42fc5dcb1df4b323c71684d5e82bcc9995808ba3
nicksavers <nicksavers@gmail.com>
2014-05-25 23:33:05 +0200

    Fix Trie to generate the expected rootHash

--- src/main/java/org/ethereum/trie/Node.java
+++ src/main/java/org/ethereum/trie/Node.java
@@ -2,12 +2,13 @@ package org.ethereum.trie;
 
 import org.ethereum.util.Value;
 
+
 /** 
  * A Node in a Merkle Patricia Tree is one of the following:
  * 
  * 	- NULL (represented as the empty string)
- * 	- A two-item array [ key, value ]
- * 	- A 17-item array [ v0 ... v15, vt ]
+ * 	- A two-item array [ key, value ] (1 key for 2-item array)
+ * 	- A 17-item array [ v0 ... v15, vt ] (16 keys for 17-item array)
  * 
  * The idea is that in the event that there is a long path of nodes 
  * each with only one element, we shortcut the descent by setting up 
@@ -21,41 +22,40 @@ import org.ethereum.util.Value;
  * 							  N     N
  * 							 / \   / \
  * 							L   L L   L
- * 
- *  
+ *  *  
  * Also, we add another conceptual change: internal nodes can no longer 
  * have values, only leaves with no children of their own can; however, 
  * since to be fully generic we want the key/value store to be able to 
  * store keys like 'dog' and 'doge' at the same time, we simply add 
  * a terminator symbol (16) to the alphabet so there is never a value 
- * "en-route" to another value. Where a node is referenced inside a node, 
- * what is included is H(rlp.encode(x)) where H(x) = sha3(x) if len(x) >= 32 else x 
- * and rlp.encode is the RLP encoding function. 
+ * "en-route" to another value. 
+ * 
+ * Where a node is referenced inside a node, what is included is:
+ * 
+ *  	H(rlp.encode(x)) where H(x) = sha3(x) if len(x) >= 32 else x 
  * 
  * Note that when updating a trie, you will need to store the key/value pair (sha3(x), x) 
  * in a persistent lookup table when you create a node with length >= 32, 
  * but if the node is shorter than that then you do not need to store anything 
  * when length < 32 for the obvious reason that the function f(x) = x is reversible.
- *
  */
 public class Node {
-	
-	private byte[] key; // 1 key for 2-item array, 16 keys for 17-item array
+
+	/* RLP encoded value of the Trie-node */
 	private Value value;
 	private boolean dirty;
 
-	public Node(byte[] key, Value val) {
-		this(key, val, false);
+	public Node(Value val) {
+		this(val, false);
 	}
 	
-	public Node(byte[] key, Value val, boolean dirty) {
-		this.key = key;
+	public Node(Value val, boolean dirty) {
 		this.value = val;
 		this.dirty = dirty;
 	}
 
 	public Node copy() {
-		return new Node(this.key.clone(), this.value, this.dirty);
+		return new Node(this.value, this.dirty);
 	}
 
 	public boolean isDirty() {
@@ -66,15 +66,7 @@ public class Node {
 		this.dirty = ditry;
 	}
 	
-	public byte[] getKey() {
-		return key;
-	}
-
 	public Value getValue() {
 		return value;
-	}
-	
-	public Object[] getItems() {
-		return new Object[] { key, value }; 
 	}
 }
--- src/test/java/org/ethereum/util/CompactEncoderTest.java
+++ src/test/java/org/ethereum/util/CompactEncoderTest.java
@@ -13,62 +13,62 @@ public class CompactEncoderTest {
 	public void testCompactEncodeOddCompact() {
 		byte[] test = new byte[] { 1, 2, 3, 4, 5 };
 		byte[] expectedData = new byte[] { 0x11, 0x23, 0x45 };
-		assertArrayEquals("odd compact encode fail", expectedData, CompactEncoder.encode(test));
+		assertArrayEquals("odd compact encode fail", expectedData, CompactEncoder.packNibbles(test));
 	}
 	
 	@Test
 	public void testCompactEncodeEvenCompact() {
 		byte[] test = new byte[] { 0, 1, 2, 3, 4, 5 };
 		byte[] expectedData = new byte[] { 0x00, 0x01, 0x23, 0x45 };
-		assertArrayEquals("even compact encode fail", expectedData, CompactEncoder.encode(test));
+		assertArrayEquals("even compact encode fail", expectedData, CompactEncoder.packNibbles(test));
 	}
 
 	@Test
 	public void testCompactEncodeEvenTerminated() {
 		byte[] test = new byte[] { 0, 15, 1, 12, 11, 8, T };
 		byte[] expectedData = new byte[] { 0x20, 0x0f, 0x1c, (byte) 0xb8 };
-		assertArrayEquals("even terminated compact encode fail", expectedData, CompactEncoder.encode(test));
+		assertArrayEquals("even terminated compact encode fail", expectedData, CompactEncoder.packNibbles(test));
 	}
 
 	@Test
 	public void testCompactEncodeOddTerminated() {		
 		byte[] test = new byte[] { 15, 1, 12, 11, 8, T };
 		byte[] expectedData = new byte[] { 0x3f, 0x1c, (byte) 0xb8 };
-		assertArrayEquals("odd terminated compact encode fail", expectedData, CompactEncoder.encode(test));
+		assertArrayEquals("odd terminated compact encode fail", expectedData, CompactEncoder.packNibbles(test));
 	}
 
 	@Test
 	public void testCompactDecodeOddCompact() {
 		byte[] test = new byte[] { 0x11, 0x23, 0x45 };
 		byte[] expected = new byte[] {1, 2, 3, 4, 5};
-		assertArrayEquals("odd compact decode fail", expected, CompactEncoder.decode(test));
+		assertArrayEquals("odd compact decode fail", expected, CompactEncoder.unpackToNibbles(test));
 	}
 
 	@Test
 	public void testCompactDecodeEvenCompact() {
 		byte[] test = new byte[] { 0x00, 0x01, 0x23, 0x45 };
 		byte[] expected = new byte[] {0, 1, 2, 3, 4, 5};
-		assertArrayEquals("even compact decode fail", expected, CompactEncoder.decode(test));
+		assertArrayEquals("even compact decode fail", expected, CompactEncoder.unpackToNibbles(test));
 	}
 
 	@Test
 	public void testCompactDecodeEvenTerminated() {
 		byte[] test = new byte[] { 0x20, 0x0f, 0x1c, (byte) 0xb8 };
 		byte[] expected = new byte[] {0, 15, 1, 12, 11, 8, T};
-		assertArrayEquals("even terminated compact decode fail", expected, CompactEncoder.decode(test));
+		assertArrayEquals("even terminated compact decode fail", expected, CompactEncoder.unpackToNibbles(test));
 	}
 
 	@Test
 	public void testCompactDecodeOddTerminated() {
 		byte[] test = new byte[] { 0x3f, 0x1c, (byte) 0xb8 };
 		byte[] expected = new byte[] {15, 1, 12, 11, 8, T};
-		assertArrayEquals("odd terminated compact decode fail", expected, CompactEncoder.decode(test));
+		assertArrayEquals("odd terminated compact decode fail", expected, CompactEncoder.unpackToNibbles(test));
 	}
 
 	@Test
 	public void testCompactHexDecode() {
 		byte[] test = "stallion".getBytes();
 		byte[] result = new byte[] { 7, 3, 7, 4, 6, 1, 6, 12, 6, 12, 6, 9, 6, 15, 6, 14, T };
-		assertArrayEquals(result, CompactEncoder.hexDecode(test));
+		assertArrayEquals(result, CompactEncoder.binToNibbles(test));
 	}
 }
