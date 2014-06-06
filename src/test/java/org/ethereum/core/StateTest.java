5442f7d59d1421e1a59dcec7eb5ff62bee1c62a0
romanman <roman.mandeleil@gmail.com>
2014-06-06 20:04:13 +0100

    finalization process: new block stateRoot calc

--- src/test/java/org/ethereum/core/StateTest.java
+++ src/test/java/org/ethereum/core/StateTest.java
@@ -89,13 +89,15 @@ public class StateTest {
     @Test  // calc state after applying first tx on genesis
     public void test2(){
 
+        // explanation:
         // 0) create genesis
         // 1) apply cost of tx to cd2a3d9f938e13cd947ec05abc7fe734df8dd826
         // 2) create AccountState for 77045e71a7a2c50903d88e564cd72fab11e82051
-        // 3) save the contract data --> sha3(data), data
+        // 3) minner gets the gas + coinbase ==> 6260000000000000 + 1500000000000000000
         // 4) calc the root
 
         Trie trie = generateGenesis();
+        String expected = "69c21ff84a5af0b53b11c61110a16d6ad43dad37b3eb29ae8e88c936eb06456a";
 
         byte[] data = trie.get(Hex.decode("cd2a3d9f938e13cd947ec05abc7fe734df8dd826"));
         AccountState account_1 = new AccountState(data);
@@ -109,16 +111,50 @@ public class StateTest {
         account_2.setCodeHash(HashUtil.sha3(codeData));
         trie.update(Hex.decode("77045e71a7a2c50903d88e564cd72fab11e82051"), account_2.getEncoded());
 
+//        this is saved in the db
 //        trie.update(HashUtil.sha3(codeData), codeData);
 
-        AccountState account_3 = new AccountState(BigInteger.ZERO, new BigInteger("3006260000000000000"));
+        AccountState account_3 = new AccountState(BigInteger.ZERO, new BigInteger("1506260000000000000"));
         trie.update(Hex.decode("4c5f4d519dff3c16f0d54b6866e256fbbbc1a600"), account_3.getEncoded());
 
-        System.out.println("" + Hex.toHexString(trie.getRootHash()));
 
-//      69c21ff84a5af0b53b11c61110a16d6ad43dad37b3eb29ae8e88c936eb06456a
-//      block 1 stateHash=69c21ff84a5af0b53b11c61110a16d6ad43dad37b3eb29ae8e88c936eb06456a
-//      block 2 stateHash=f2ae210b7141ba737fc8e356d539874e57b4aa65a5072aebbf4c53fd4db77b55
+        assertEquals(expected,  Hex.toHexString(trie.getRootHash()));
+
+
+        /* *** GROSS DATA ***
+
+        BlockData [
+          hash=22cf863ab836a6f5c29389d2e77f4792a3b3b52908c98ed14b1cbe91491a3e36
+          parentHash=77ef4fdaf389dca53236bcf7f72698e154eab2828f86fbc4fc6cd9225d285c89
+          unclesHash=1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347
+          coinbase=4c5f4d519dff3c16f0d54b6866e256fbbbc1a600
+          stateHash=69c21ff84a5af0b53b11c61110a16d6ad43dad37b3eb29ae8e88c936eb06456a
+          txTrieHash=a77691cf47bec9021d3f027fc8ef2d51b758b600a79967154354b8e37108896f
+          difficulty=3ff000
+          number=1
+          minGasPrice=10000000000000
+          gasLimit=999023
+          gasUsed=626
+          timestamp=1401979976 (2014.06.05 15:52:56)
+          extraData=null
+          nonce=0000000000000000000000000000000000000000000000005d439960040e4505
+
+        TransactionReceipt[
+           TransactionData [ hash=1ee6fa3149a5e9c09b54009eb6e108aaa7ecd79483d57eedcf2dff93a1505588  nonce=null,
+               gasPrice=09184e72a000, gas=03e8, receiveAddress=0000000000000000000000000000000000000000, value=03e8,
+               data=60016000546006601160003960066000f261778e600054, signatureV=27,
+               signatureR=2b379f22050e3554c3fa5423d9040bb28dcc7f905300db4e67c03bcf9b27003c,
+               signatureS=59f47793e050974e6b5fca2848b19925637b883a012693b54d712f1c4f74def5
+          ]
+          , postTxState=7fa5bd00f6e03b5a5718560f1e25179b227167585a3c3da06a48f554365fb527
+          , cumulativeGas=0272]
+        ]
+
+        +++  4c5f4d519dff3c16f0d54b6866e256fbbbc1a600:
+        +++  77045e71a7a2c50903d88e564cd72fab11e82051: $[61,77,8e,60,0,54] ([])
+         *   cd2a3d9f938e13cd947ec05abc7fe734df8dd826: #1 1606938044258990275541962092341162602522202987522792835300376 (-6260000000001000)
+          */
+
 
     }
 
