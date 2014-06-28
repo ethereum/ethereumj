c82a929d303cd9377f8c72fa3dd76ba9c825d7f2
nicksavers <nicksavers@gmail.com>
2014-06-28 13:18:43 +0200

    Delete temp database after unit test

--- ethereumj-core/src/test/java/org/ethereum/db/TrackDatabaseTest.java
+++ ethereumj-core/src/test/java/org/ethereum/db/TrackDatabaseTest.java
@@ -1,6 +1,15 @@
 package org.ethereum.db;
 
-import org.junit.Assert;
+import static org.iq80.leveldb.impl.Iq80DBFactory.factory;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.iq80.leveldb.Options;
+import org.junit.AfterClass;
+
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.spongycastle.util.encoders.Hex;
 
@@ -11,7 +20,7 @@ import org.spongycastle.util.encoders.Hex;
  * Created on: 11/06/2014 14:54
  */
 public class TrackDatabaseTest {
-
+	
     @Test
     public void test1() {
 
@@ -20,23 +29,33 @@ public class TrackDatabaseTest {
 
         trackDatabase1.put(Hex.decode("abcdef"), Hex.decode("abcdef"));
         byte[] value  = trackDatabase1.get(Hex.decode("abcdef"));
-        Assert.assertEquals("abcdef", Hex.toHexString(value));
+        assertEquals("abcdef", Hex.toHexString(value));
 
         trackDatabase1.startTrack();
         trackDatabase1.put(Hex.decode("abcdef"), Hex.decode("ffffff"));
         value  = trackDatabase1.get(Hex.decode("abcdef"));
-        Assert.assertEquals("ffffff", Hex.toHexString(value));
+        assertEquals("ffffff", Hex.toHexString(value));
 
         trackDatabase1.rollbackTrack();
         value  = trackDatabase1.get(Hex.decode("abcdef"));
-        Assert.assertEquals("abcdef", Hex.toHexString(value));
+        assertEquals("abcdef", Hex.toHexString(value));
 
         trackDatabase1.startTrack();
         trackDatabase1.put(Hex.decode("abcdef"), Hex.decode("ffffff"));
         trackDatabase1.commitTrack();
         value  = trackDatabase1.get(Hex.decode("abcdef"));
-        Assert.assertEquals("ffffff", Hex.toHexString(value));
+        assertEquals("ffffff", Hex.toHexString(value));
 
         db1.close();
     }
+    
+    @AfterClass
+	public static void destroyDB() {
+		try {
+			Options options = new Options();
+			factory.destroy(new File("temp"), options);
+		} catch (IOException e) {
+			fail("Destroying temp-db failed");
+		}
+	}
 }
