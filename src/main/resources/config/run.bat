51d24eb8c014bb45988940f27530a8d4325f3885
romanman <roman.mandeleil@gmail.com>
2014-07-07 11:45:37 +0100

    added minimu heap size param

--- src/main/resources/config/run.bat
+++ src/main/resources/config/run.bat
@@ -1,2 +1,2 @@
-call javaw -Dlog4j.configuration=file:./config/log4j.properties -jar ethereumj.jar
+call javaw -Xmx6144M -Dlog4j.configuration=file:./config/log4j.properties -jar ethereumj.jar
 pause
--- src/main/resources/config/run.sh
+++ src/main/resources/config/run.sh
@@ -1,1 +1,1 @@
-java -Dlog4j.configuration=file:./config/log4j.properties -jar ethereumj.jar
+java -Xmx6144M -Dlog4j.configuration=file:./config/log4j.properties -jar ethereumj.jar
