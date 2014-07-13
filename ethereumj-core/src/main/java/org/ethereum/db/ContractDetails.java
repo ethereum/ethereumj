bc24645644dc49e950ea534505d63b1064e70db0
nicksavers <nicksavers@gmail.com>
2014-07-13 23:05:04 +0200

    Add namecoin.json and fix in TestRunner

--- ethereumj-core/src/main/java/org/ethereum/db/ContractDetails.java
+++ ethereumj-core/src/main/java/org/ethereum/db/ContractDetails.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.ethereum.crypto.SHA3Helper;
 import org.ethereum.trie.Trie;
 import org.ethereum.util.RLP;
 import org.ethereum.util.RLPElement;
