0bb70d84a3cf1d95fa317a4484f2d14b2e6b54b7
nicksavers <nicksavers@gmail.com>
2014-10-10 13:37:54 +0200

    Remove typo

--- ethereumj-core/src/main/java/org/ethereum/util/Utils.java
+++ ethereumj-core/src/main/java/org/ethereum/util/Utils.java
@@ -8,7 +8,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.regex.Pattern;
-w
 import javax.swing.ImageIcon;
 
 import org.spongycastle.util.encoders.DecoderException;
