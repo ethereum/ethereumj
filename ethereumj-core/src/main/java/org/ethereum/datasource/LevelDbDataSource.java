34210620f2447230734d037f2569d112a2cb033d
romanman <roman@ethergit.com>
2015-06-18 10:33:27 +0300

    Remove JNI support

--- ethereumj-core/src/main/java/org/ethereum/datasource/LevelDbDataSource.java
+++ ethereumj-core/src/main/java/org/ethereum/datasource/LevelDbDataSource.java
@@ -2,15 +2,12 @@ package org.ethereum.datasource;
 
 import org.ethereum.config.SystemProperties;
 
-import org.fusesource.leveldbjni.JniDBFactory;
-import org.fusesource.leveldbjni.internal.JniDB;
 import org.iq80.leveldb.CompressionType;
 import org.iq80.leveldb.DB;
 import org.iq80.leveldb.DBIterator;
 import org.iq80.leveldb.Options;
 import org.iq80.leveldb.WriteBatch;
 
-import org.iq80.leveldb.impl.Iq80DBFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -64,23 +61,7 @@ public class LevelDbDataSource implements KeyValueDataSource {
             }
 
             logger.debug("Initializing new or existing database: '{}'", name);
-
-            try {
-                db = JniDBFactory.factory.open(fileLocation, options);
-            } catch (Throwable e) {
-                System.out.println("No native version of LevelDB found");
-            }
-
-            String cpu = System.getProperty("sun.arch.data.model");
-            String os = System.getProperty("os.name");
-
-            if (db instanceof JniDB)
-                System.out.println("Native version of LevelDB loaded for: " + os + "." + cpu + "bit");
-            else{
-                System.out.println("Pure Java version of LevelDB loaded");
-                db = Iq80DBFactory.factory.open(fileLocation, options);
-            }
-
+            db = factory.open(fileLocation, options);
 
         } catch (IOException ioe) {
             logger.error(ioe.getMessage(), ioe);
