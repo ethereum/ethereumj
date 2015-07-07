6c5f162485264a8aa5a97f4ec2398aae0a49d2d3
Mikhail Kalinin <kalinin128@gmail.com>
2015-07-07 19:07:16 +0600

    Split DataSource interface and KeyValue interface

---
+++ ethereumj-core/src/main/java/org/ethereum/datasource/DataSource.java
@@ -0,0 +1,14 @@
+package org.ethereum.datasource;
+
+/**
+ * @author Mikhail Kalinin
+ * @since 07.07.2015
+ */
+public interface DataSource {
+
+    void init();
+
+    void setName(String name);
+
+    void close();
+}
