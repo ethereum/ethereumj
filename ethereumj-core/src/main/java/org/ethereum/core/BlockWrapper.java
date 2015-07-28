bee061cf2db195ddc40bc9598543e3e6066fa27a
Mikhail Kalinin <kalinin128@gmail.com>
2015-07-28 19:47:35 +0600

    Provide BlockWrapper with receivedAt field

--- ethereumj-core/src/main/java/org/ethereum/core/BlockWrapper.java
+++ ethereumj-core/src/main/java/org/ethereum/core/BlockWrapper.java
@@ -13,6 +13,7 @@ public class BlockWrapper {
 
     private Block block;
     private long importFailedAt = 0;
+    private long receivedAt = 0;
     private boolean newBlock;
 
     public BlockWrapper(Block block) {
@@ -60,6 +61,14 @@ public class BlockWrapper {
         return block.getParentHash();
     }
 
+    public long getReceivedAt() {
+        return receivedAt;
+    }
+
+    public void setReceivedAt(long receivedAt) {
+        this.receivedAt = receivedAt;
+    }
+
     public void importFailed() {
         if (importFailedAt == 0) {
             importFailedAt = System.currentTimeMillis();
@@ -77,11 +86,19 @@ public class BlockWrapper {
     public byte[] getBytes() {
         byte[] blockBytes = block.getEncoded();
         byte[] importFailedBytes = BigInteger.valueOf(importFailedAt).toByteArray();
-        byte[] bytes = new byte[blockBytes.length + importFailedBytes.length + 2];
+        byte[] receivedAtBytes = BigInteger.valueOf(receivedAt).toByteArray();
+
+        byte[] bytes = new byte[blockBytes.length + importFailedBytes.length + receivedAtBytes.length + 3];
+
         bytes[0] = (byte) (newBlock ? 1 : 0);
+
         bytes[1] = (byte) importFailedBytes.length;
         System.arraycopy(importFailedBytes, 0, bytes, 2, importFailedBytes.length);
-        System.arraycopy(blockBytes, 0, bytes, importFailedBytes.length + 2, blockBytes.length);
+
+        bytes[importFailedBytes.length + 2] = (byte) receivedAtBytes.length;
+        System.arraycopy(receivedAtBytes, 0, bytes, importFailedBytes.length + 3, receivedAtBytes.length);
+
+        System.arraycopy(blockBytes, 0, bytes, importFailedBytes.length + receivedAtBytes.length + 3, blockBytes.length);
         return bytes;
     }
 
@@ -89,8 +106,11 @@ public class BlockWrapper {
         byte[] importFailedAtBytes = new byte[bytes[1]];
         System.arraycopy(bytes, 2, importFailedAtBytes, 0, importFailedAtBytes.length);
 
-        byte[] blockBytes = new byte[bytes.length - importFailedAtBytes.length - 2];
-        System.arraycopy(bytes, importFailedAtBytes.length + 2, blockBytes, 0, blockBytes.length);
+        byte[] receivedAtBytes = new byte[bytes[importFailedAtBytes.length + 2]];
+        System.arraycopy(bytes, importFailedAtBytes.length + 3, receivedAtBytes, 0, receivedAtBytes.length);
+
+        byte[] blockBytes = new byte[bytes.length - importFailedAtBytes.length - receivedAtBytes.length - 3];
+        System.arraycopy(bytes, importFailedAtBytes.length + importFailedAtBytes.length + 3, blockBytes, 0, blockBytes.length);
 
         this.newBlock = bytes[0] == 1;
         this.importFailedAt = new BigInteger(importFailedAtBytes).longValue();
