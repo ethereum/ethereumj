b96c663952f5d46755d47ff7fd153b4ad7c3fa61
Mikhail Kalinin <kalinin128@gmail.com>
2015-08-28 11:38:08 +0600

    Refactor fork coverage flow

--- ethereumj-core/src/main/java/org/ethereum/net/eth/handler/Eth61.java
+++ ethereumj-core/src/main/java/org/ethereum/net/eth/handler/Eth61.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -61,53 +60,21 @@ public class Eth61 extends EthHandler {
         }
 
         if (!forkCovered) {
-
-            if (lastAskedNumber <= 1) {
-                forkCovered = true;
-                sendGetBlockHashesByNumber(1, maxHashesAsk);
-                return;
-            }
-
-            // start downloading from blockNumber of the block with known hash
-            ListIterator<byte[]> it = received.listIterator(received.size());
-            while (it.hasPrevious()) {
-                byte[] hash = it.previous();
-                if (blockchain.isBlockExist(hash)) {
-                    forkCovered = true;
-                    Block block = blockchain.getBlockByHash(hash);
-                    sendGetBlockHashesByNumber(block.getNumber() + 1, maxHashesAsk);
-                    return;
-                }
-            }
-
-            logger.trace("Peer {}: fork is not covered yet");
-
-            long blockNumber = max(1, lastAskedNumber - FORK_COVER_BATCH_SIZE);
-            sendGetBlockHashesByNumber(blockNumber, FORK_COVER_BATCH_SIZE);
-
+            maintainForkCoverage(received);
             return;
-        }
-
-        boolean foundTerminal = false;
-        for(byte[] hash : received) {
-            if (Arrays.equals(hash, lastHashToAsk)) {
-                foundTerminal = true;
-            }
         }
 
         queue.addHashesLast(received);
 
-        if (foundTerminal) {
-            changeState(DONE_HASH_RETRIEVING);
-            logger.trace(
-                    "Peer {}: got terminal hash [{}]",
-                    channel.getPeerIdShort(),
-                    Hex.toHexString(lastHashToAsk)
-            );
-        } else {
-            long blockNumber = lastAskedNumber + min(received.size(), maxHashesAsk);
-            sendGetBlockHashesByNumber(blockNumber, maxHashesAsk);
-        }
+        for(byte[] hash : received)
+            if (Arrays.equals(hash, lastHashToAsk)) {
+                changeState(DONE_HASH_RETRIEVING);
+                logger.trace("Peer {}: got terminal hash [{}]", channel.getPeerIdShort(), Hex.toHexString(lastHashToAsk));
+                return;
+            }
+
+        long blockNumber = lastAskedNumber + min(received.size(), maxHashesAsk);
+        sendGetBlockHashesByNumber(blockNumber, maxHashesAsk);
     }
 
     private void sendGetBlockHashesByNumber(long blockNumber, int maxHashesAsk) {
@@ -138,24 +105,72 @@ public class Eth61 extends EthHandler {
     @Override
     protected void startHashRetrieving() {
 
-        long blockNumber = blockchain.getBestBlock().getNumber() + 1;
+        forkCovered = true;
+        long bestNumber = blockchain.getBestBlock().getNumber();
 
-        if (blockNumber > 1) {
-
-            logger.trace("Peer {}: check if our chain is on the fork", channel.getPeerIdShort());
+        if (bestNumber > 0) {
 
             // always assume we're on the fork if best block is not a Genesis one
-            forkCovered = false;
-            blockNumber = max(1, blockNumber - FORK_COVER_BATCH_SIZE);
-            sendGetBlockHashesByNumber(blockNumber, FORK_COVER_BATCH_SIZE);
+            startForkCoverage();
 
         } else {
 
             // if we're at the beginning there can't be any fork
-            forkCovered = true;
-            sendGetBlockHashesByNumber(blockNumber, maxHashesAsk);
+            sendGetBlockHashesByNumber(bestNumber + 1, maxHashesAsk);
 
         }
 
+    }
+
+    /************************
+    *     Fork Coverage     *
+    *************************/
+
+
+    private void startForkCoverage() {
+
+        forkCovered = false;
+
+        logger.trace("Peer {}: check if our chain is on the fork", channel.getPeerIdShort());
+
+        long bestNumber = blockchain.getBestBlock().getNumber();
+        long blockNumber = max(1, bestNumber - FORK_COVER_BATCH_SIZE);
+        sendGetBlockHashesByNumber(blockNumber, FORK_COVER_BATCH_SIZE);
+
+    }
+
+    private void maintainForkCoverage(List<byte[]> received) {
+
+        long blockNumber = max(1, lastAskedNumber - FORK_COVER_BATCH_SIZE);
+
+        if (lastAskedNumber > 1) {
+
+            // start downloading hashes from blockNumber of the block with known hash
+            ListIterator<byte[]> it = received.listIterator(received.size());
+            while (it.hasPrevious()) {
+                byte[] hash = it.previous();
+                if (blockchain.isBlockExist(hash)) {
+                    forkCovered = true;
+                    Block block = blockchain.getBlockByHash(hash);
+                    blockNumber = block.getNumber() + 1;
+                }
+            }
+
+        } else {
+            forkCovered = true;
+        }
+
+        if (forkCovered) {
+
+            // start hash sync
+            sendGetBlockHashesByNumber(blockNumber, maxHashesAsk);
+
+        } else {
+
+            // continue fork coverage
+            logger.trace("Peer {}: fork is not covered yet");
+            sendGetBlockHashesByNumber(blockNumber, FORK_COVER_BATCH_SIZE);
+
+        }
     }
 }
