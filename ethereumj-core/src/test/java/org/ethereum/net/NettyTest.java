72eac2f383eadc1c04c58a3f5a79d5260cbb47f2
Anton Nahsatyrev <anton.nashatyrev@gmail.com>
2015-10-16 18:34:01 +0300

    Extend NettyTest

--- ethereumj-core/src/test/java/org/ethereum/net/NettyTest.java
+++ ethereumj-core/src/test/java/org/ethereum/net/NettyTest.java
@@ -2,10 +2,13 @@ package org.ethereum.net;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.*;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.MessageToMessageCodec;
+import io.netty.handler.codec.MessageToMessageDecoder;
+import io.netty.handler.codec.MessageToMessageEncoder;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -14,34 +17,99 @@ import java.util.List;
 /**
  * Created by Anton Nashatyrev on 16.10.2015.
  */
-@Ignore
 public class NettyTest {
     @Test
     public void pipelineTest() {
 
-        ByteToMessageDecoder decoder1 = new ByteToMessageDecoder() {
-            @Override
-            protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-                int i = in.readInt();
-                System.out.println("decoder1 read int (4 bytes). Needs no more: " + Integer.toHexString(i));
-                ctx.pipeline().remove(this);
-            }
-        };
+        final int[] int2 = new int[1];
+        final boolean[] exception = new boolean[1];
 
-        ByteToMessageDecoder decoder2 = new ByteToMessageDecoder() {
+        final ByteToMessageDecoder decoder2 = new ByteToMessageDecoder() {
             @Override
             protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
                 int i = in.readInt();
                 System.out.println("decoder2 read int (4 bytes): " + Integer.toHexString(i));
+                int2[0] = i;
+                if (i == 0) out.add("aaa");
+            }
+            @Override
+            public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                System.out.println("Decoder2 exception: " + cause);
             }
         };
 
-        EmbeddedChannel channel = new EmbeddedChannel(decoder1, decoder2);
+        final MessageToMessageCodec decoder3 = new MessageToMessageCodec<Object, Object>() {
+            @Override
+            protected void decode(ChannelHandlerContext ctx, Object msg, List<Object> out) throws Exception {
+                System.out.println("NettyTest.decode: msg = [" + msg + "]");
+                if (msg == "aaa") {
+                    throw new RuntimeException("Test exception 3");
+                }
+            }
+
+            @Override
+            protected void encode(ChannelHandlerContext ctx, Object msg, List<Object> out) throws Exception {
+                throw new RuntimeException("Test exception 4");
+            }
+
+            @Override
+            public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                System.out.println("Decoder3 exception: " + cause);
+                exception[0] = true;
+            }
+        };
+
+        final ByteToMessageDecoder decoder1 = new ByteToMessageDecoder() {
+            @Override
+            protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+                int i = in.readInt();
+                System.out.println("decoder1 read int (4 bytes). Needs no more: " + Integer.toHexString(i));
+                ctx.pipeline().addAfter("decoder1", "decoder2", decoder2);
+                ctx.pipeline().addAfter("decoder2", "decoder3", decoder3);
+                ctx.pipeline().remove(this);
+            }
+
+            @Override
+            public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                System.out.println("Decoder1 exception: " + cause);
+            }
+        };
+
+        ChannelInboundHandlerAdapter initiator = new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                ctx.pipeline().addFirst("decoder1", decoder1);
+                System.out.println("NettyTest.channelActive");
+            }
+        };
+
+        EmbeddedChannel channel0 = new EmbeddedChannel(new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                throw new RuntimeException("Test");
+            }
+            @Override
+            public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                System.out.println("Exception caught: " + cause);
+            }
+
+        });
+        EmbeddedChannel channel = new EmbeddedChannel(initiator);
         ByteBuf buffer = Unpooled.buffer();
         buffer.writeInt(0x12345678);
         buffer.writeInt(0xabcdefff);
         channel.writeInbound(buffer);
+        Assert.assertEquals(0xabcdefff, int2[0]);
 
+        channel.writeInbound(Unpooled.buffer().writeInt(0));
+        Assert.assertTrue(exception[0]);
+
+        // Need the following for the exception in outbound handler to be fired
+        // ctx.writeAndFlush(msg).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+
+//        exception[0] = false;
+//        channel.writeOutbound("outMsg");
+//        Assert.assertTrue(exception[0]);
     }
 
 }
