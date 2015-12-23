e59d945f113cc78a72779d078c4bcde576c0a67f
Anton Nahsatyrev <anton.nashatyrev@gmail.com>
2015-12-23 15:53:50 +0300

    Make LevelDB close() operation thread-safe. Add more trace 'db' logs

--- ethereumj-core/src/main/java/org/ethereum/datasource/LevelDbDataSource.java
+++ ethereumj-core/src/main/java/org/ethereum/datasource/LevelDbDataSource.java
@@ -4,6 +4,7 @@ import org.ethereum.config.SystemProperties;
 import org.iq80.leveldb.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spongycastle.util.encoders.Hex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,8 @@ import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import static java.lang.System.getProperty;
 import static org.ethereum.config.SystemProperties.CONFIG;
@@ -38,40 +41,57 @@ public class LevelDbDataSource implements KeyValueDataSource {
     DB db;
     boolean alive;
 
+    // The native LevelDB insert/update/delete are normally thread-safe
+    // However close operation is not thread-safe and may lead to a native crash when
+    // accessing a closed DB.
+    // The leveldbJNI lib has a protection over accessing closed DB but it is not synchronized
+    // This ReadWriteLock still permits concurrent execution of insert/delete/update operations
+    // however blocks them on init/close/delete operations
+    private ReadWriteLock resetDbLock = new ReentrantReadWriteLock();
+
     public LevelDbDataSource() {
     }
 
     public LevelDbDataSource(String name) {
         this.name = name;
+        logger.info("New LevelDbDataSource: " + name);
     }
 
     @Override
     public void init() {
-        if (isAlive()) return;
-
-        if (name == null) throw new NullPointerException("no name set to the db");
-
-        Options options = new Options();
-        options.createIfMissing(true);
-        options.compressionType(CompressionType.NONE);
-        options.blockSize(10 * 1024 * 1024);
-        options.writeBufferSize(10 * 1024 * 1024);
-        options.cacheSize(0);
-        options.paranoidChecks(true);
-        options.verifyChecksums(true);
-
+        resetDbLock.writeLock().lock();
         try {
-            logger.debug("Opening database");
-            Path dbPath = Paths.get(getProperty("user.dir"), config.databaseDir(), name);
-            Files.createDirectories(dbPath.getParent());
+            logger.debug("~> LevelDbDataSource.init(): " + name);
 
-            logger.debug("Initializing new or existing database: '{}'", name);
-            db = factory.open(dbPath.toFile(), options);
+            if (isAlive()) return;
 
-            alive = true;
-        } catch (IOException ioe) {
-            logger.error(ioe.getMessage(), ioe);
-            throw new RuntimeException("Can't initialize database");
+            if (name == null) throw new NullPointerException("no name set to the db");
+
+            Options options = new Options();
+            options.createIfMissing(true);
+            options.compressionType(CompressionType.NONE);
+            options.blockSize(10 * 1024 * 1024);
+            options.writeBufferSize(10 * 1024 * 1024);
+            options.cacheSize(0);
+            options.paranoidChecks(true);
+            options.verifyChecksums(true);
+
+            try {
+                logger.debug("Opening database");
+                Path dbPath = Paths.get(getProperty("user.dir"), config.databaseDir(), name);
+                Files.createDirectories(dbPath.getParent());
+
+                logger.debug("Initializing new or existing database: '{}'", name);
+                db = factory.open(dbPath.toFile(), options);
+
+                alive = true;
+            } catch (IOException ioe) {
+                logger.error(ioe.getMessage(), ioe);
+                throw new RuntimeException("Can't initialize database");
+            }
+            logger.debug("<~ LevelDbDataSource.init(): " + name);
+        } finally {
+            resetDbLock.writeLock().unlock();
         }
     }
 
@@ -81,12 +101,17 @@ public class LevelDbDataSource implements KeyValueDataSource {
     }
 
     public void destroyDB(File fileLocation) {
-        logger.debug("Destroying existing database");
-        Options options = new Options();
+        resetDbLock.writeLock().lock();
         try {
-            factory.destroy(fileLocation, options);
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
+            logger.debug("Destroying existing database: " + fileLocation);
+            Options options = new Options();
+            try {
+                factory.destroy(fileLocation, options);
+            } catch (IOException e) {
+                logger.error(e.getMessage(), e);
+            }
+        } finally {
+            resetDbLock.writeLock().unlock();
         }
     }
 
@@ -102,34 +127,67 @@ public class LevelDbDataSource implements KeyValueDataSource {
 
     @Override
     public byte[] get(byte[] key) {
+        resetDbLock.readLock().lock();
         try {
-            return db.get(key);
-        } catch (DBException e) {
-            return db.get(key);
+            if (logger.isTraceEnabled()) logger.trace("~> LevelDbDataSource.get(): " + name + ", key: " + Hex.toHexString(key));
+            try {
+                byte[] ret = db.get(key);
+                if (logger.isTraceEnabled()) logger.trace("<~ LevelDbDataSource.get(): " + name + ", key: " + Hex.toHexString(key) + ", " + (ret == null ? "null" : ret.length));
+                return ret;
+            } catch (DBException e) {
+                logger.error("Exception. Retrying again...", e);
+                byte[] ret = db.get(key);
+                if (logger.isTraceEnabled()) logger.trace("<~ LevelDbDataSource.get(): " + name + ", key: " + Hex.toHexString(key) + ", " + (ret == null ? "null" : ret.length));
+                return ret;
+            }
+        } finally {
+            resetDbLock.readLock().unlock();
         }
     }
 
     @Override
     public byte[] put(byte[] key, byte[] value) {
-        db.put(key, value);
-        return value;
+        resetDbLock.readLock().lock();
+        try {
+            if (logger.isTraceEnabled()) logger.trace("~> LevelDbDataSource.put(): " + name + ", key: " + Hex.toHexString(key) + ", " + (value == null ? "null" : value.length));
+            db.put(key, value);
+            if (logger.isTraceEnabled()) logger.trace("<~ LevelDbDataSource.put(): " + name + ", key: " + Hex.toHexString(key) + ", " + (value == null ? "null" : value.length));
+            return value;
+        } finally {
+            resetDbLock.readLock().unlock();
+        }
     }
 
     @Override
     public void delete(byte[] key) {
-        db.delete(key);
+        resetDbLock.readLock().lock();
+        try {
+            if (logger.isTraceEnabled()) logger.trace("~> LevelDbDataSource.delete(): " + name + ", key: " + Hex.toHexString(key));
+            db.delete(key);
+            if (logger.isTraceEnabled()) logger.trace("<~ LevelDbDataSource.delete(): " + name + ", key: " + Hex.toHexString(key));
+        } finally {
+            resetDbLock.readLock().unlock();
+        }
     }
 
     @Override
     public Set<byte[]> keys() {
-        try (DBIterator iterator = db.iterator()) {
-            Set<byte[]> result = new HashSet<>();
-            for (iterator.seekToFirst(); iterator.hasNext(); iterator.next()) {
-                result.add(iterator.peekNext().getKey());
+        resetDbLock.readLock().lock();
+        try {
+            if (logger.isTraceEnabled()) logger.trace("~> LevelDbDataSource.keys(): " + name);
+            try (DBIterator iterator = db.iterator()) {
+                Set<byte[]> result = new HashSet<>();
+                for (iterator.seekToFirst(); iterator.hasNext(); iterator.next()) {
+                    result.add(iterator.peekNext().getKey());
+                }
+                if (logger.isTraceEnabled()) logger.trace("<~ LevelDbDataSource.keys(): " + name + ", " + result.size());
+                return result;
+            } catch (IOException e) {
+                logger.error("Unexpected", e);
+                throw new RuntimeException(e);
             }
-            return result;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } finally {
+            resetDbLock.readLock().unlock();
         }
     }
 
@@ -144,29 +202,44 @@ public class LevelDbDataSource implements KeyValueDataSource {
 
     @Override
     public void updateBatch(Map<byte[], byte[]> rows) {
-        try  {
-            updateBatchInternal(rows);
-        } catch (Exception e) {
-            // try one more time
+        resetDbLock.readLock().lock();
+        try {
+            if (logger.isTraceEnabled()) logger.trace("~> LevelDbDataSource.updateBatch(): " + name + ", " + rows.size());
             try {
                 updateBatchInternal(rows);
-            } catch (Exception e1) {
-                throw new RuntimeException(e);
+                if (logger.isTraceEnabled()) logger.trace("<~ LevelDbDataSource.updateBatch(): " + name + ", " + rows.size());
+            } catch (Exception e) {
+                logger.error("Error, retrying one more time...", e);
+                // try one more time
+                try {
+                    updateBatchInternal(rows);
+                    if (logger.isTraceEnabled()) logger.trace("<~ LevelDbDataSource.updateBatch(): " + name + ", " + rows.size());
+                } catch (Exception e1) {
+                    logger.error("Error", e);
+                    throw new RuntimeException(e);
+                }
             }
+        } finally {
+            resetDbLock.readLock().unlock();
         }
     }
 
     @Override
     public void close() {
-        if (!isAlive()) return;
-
+        resetDbLock.writeLock().lock();
         try {
-            logger.debug("Close db: {}", name);
-            db.close();
+            if (!isAlive()) return;
 
-            alive = false;
-        } catch (IOException e) {
-            logger.error("Failed to find the db file on the close: {} ", name);
+            try {
+                logger.debug("Close db: {}", name);
+                db.close();
+
+                alive = false;
+            } catch (IOException e) {
+                logger.error("Failed to find the db file on the close: {} ", name);
+            }
+        } finally {
+            resetDbLock.writeLock().unlock();
         }
     }
 }
