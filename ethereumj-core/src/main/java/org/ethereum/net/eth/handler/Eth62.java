d529394ad8052a426cdef09e869d72f4348b9c16
Anton Nahsatyrev <anton.nashatyrev@gmail.com>
2016-03-31 23:55:22 +0300

    Fix IndexOutOfBounds exception (the Bodies response can come after all the headers are returned back)

--- ethereumj-core/src/main/java/org/ethereum/net/eth/handler/Eth62.java
+++ ethereumj-core/src/main/java/org/ethereum/net/eth/handler/Eth62.java
@@ -867,9 +867,10 @@ public class Eth62 extends EthHandler {
         if (!syncDone) {
 
             int expectedCount = 0;
-            if (sentHeaders.get(sentHeaders.size() - 1).getNumber() <= bestKnownBlock.getNumber()) {
+            if (sentHeaders.size() > 0 &&
+                    sentHeaders.get(sentHeaders.size() - 1).getNumber() <= bestKnownBlock.getNumber()) {
                 expectedCount = sentHeaders.size();
-            } else if (sentHeaders.get(0).getNumber() > bestKnownBlock.getNumber()) {
+            } else if (sentHeaders.size() > 0 && sentHeaders.get(0).getNumber() > bestKnownBlock.getNumber()) {
                 expectedCount = 0;
             } else {
                 for (int i = 0; i < sentHeaders.size(); i++)
