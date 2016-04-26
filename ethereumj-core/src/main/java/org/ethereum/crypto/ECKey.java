1b1be96c245eaef48d151014aa18f29b64d15fe6
Daniel James <dwhjames@gmail.com>
2016-04-26 13:05:50 -0400

    code review: fix accessors to private EC scalar value
    Be consistent in using the BouncyCastle method
    to access the private scalar value, rather than
    mixing use with the Java security interface.
    
    see: https://github.com/ethereum/ethereumj/pull/363#discussion_r61016528

--- ethereumj-core/src/main/java/org/ethereum/crypto/ECKey.java
+++ ethereumj-core/src/main/java/org/ethereum/crypto/ECKey.java
@@ -519,7 +519,7 @@ public class ECKey implements Serializable {
         if (privKey == null) {
             throw new MissingPrivateKeyException();
         } else if (privKey instanceof BCECPrivateKey) {
-            return ((BCECPrivateKey) privKey).getS();
+            return ((BCECPrivateKey) privKey).getD();
         } else {
             throw new MissingPrivateKeyException();
         }
@@ -552,7 +552,7 @@ public class ECKey implements Serializable {
         StringBuilder b = new StringBuilder();
         b.append(toString());
         if (privKey != null && privKey instanceof BCECPrivateKey) {
-            b.append(" priv:").append(Hex.toHexString(((BCECPrivateKey) privKey).getS().toByteArray()));
+            b.append(" priv:").append(Hex.toHexString(((BCECPrivateKey) privKey).getD().toByteArray()));
         }
         return b.toString();
     }
@@ -1144,7 +1144,7 @@ public class ECKey implements Serializable {
         if (privKey == null) {
             return null;
         } else if (privKey instanceof BCECPrivateKey) {
-            return bigIntegerToBytes(((BCECPrivateKey) privKey).getS(), 32);
+            return bigIntegerToBytes(((BCECPrivateKey) privKey).getD(), 32);
         } else {
             return null;
         }
