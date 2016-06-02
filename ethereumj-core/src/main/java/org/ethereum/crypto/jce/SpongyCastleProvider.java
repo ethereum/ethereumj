c8a01ca305f5d2c1954b93233c4e40736dfbb124
Daniel James <dwhjames@gmail.com>
2016-06-01 21:52:32 -0400

    revise commit 41cf427
    another attempt at PR #10
    Fix classloading issues with the Java
    Security Providers system by ditching use
    of the global provider map.

--- ethereumj-core/src/main/java/org/ethereum/crypto/jce/SpongyCastleProvider.java
+++ ethereumj-core/src/main/java/org/ethereum/crypto/jce/SpongyCastleProvider.java
@@ -3,25 +3,11 @@ package org.ethereum.crypto.jce;
 import org.spongycastle.jce.provider.BouncyCastleProvider;
 
 import java.security.Provider;
-import java.security.Security;
 
 public final class SpongyCastleProvider {
 
-  private static final String PROVIDER_NAME = BouncyCastleProvider.PROVIDER_NAME;
-
   private static class Holder {
-    private static final Provider INSTANCE;
-
-    static {
-      Provider provider = Security.getProvider(PROVIDER_NAME);
-
-      if (provider == null) {
-        INSTANCE = new BouncyCastleProvider();
-        Security.addProvider(INSTANCE);
-      } else {
-        INSTANCE = provider;
-      }
-    }
+    private static final Provider INSTANCE = new BouncyCastleProvider();
   }
 
   public static Provider getInstance() {
