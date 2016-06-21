611fdd26788845a5c184eabbbfeeffd1d96cc7bc
Anton Nahsatyrev <anton.nashatyrev@gmail.com>
2016-06-21 21:59:22 +0300

    Print StackOverflowError to log

--- ethereumj-core/src/main/java/org/ethereum/vm/VM.java
+++ ethereumj-core/src/main/java/org/ethereum/vm/VM.java
@@ -1186,7 +1186,7 @@ public class VM {
         } catch (RuntimeException e) {
             program.setRuntimeFailure(e);
         } catch (StackOverflowError soe){
-            logger.error("\n !!! StackOverflowError: update your java run command with -Xss32M !!!\n");
+            logger.error("\n !!! StackOverflowError: update your java run command with -Xss32M !!!\n", soe);
             System.exit(-1);
         }
     }
