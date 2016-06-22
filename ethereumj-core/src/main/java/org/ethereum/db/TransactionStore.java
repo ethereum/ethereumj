0baead722d16f304554d18964360de355f2e1247
Anton Nahsatyrev <anton.nashatyrev@gmail.com>
2016-06-22 23:04:02 +0300

    Store transaction receipts for all blocks where transaction was included

--- ethereumj-core/src/main/java/org/ethereum/db/TransactionStore.java
+++ ethereumj-core/src/main/java/org/ethereum/db/TransactionStore.java
@@ -2,27 +2,77 @@ package org.ethereum.db;
 
 import org.ethereum.datasource.*;
 import org.ethereum.core.TransactionInfo;
+import org.ethereum.util.ByteUtil;
+import org.ethereum.util.FastByteComparisons;
+import org.ethereum.util.RLP;
+import org.ethereum.util.RLPList;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * Storage (tx hash) => (block idx, tx idx, TransactionReceipt)
+ * Storage (tx hash) => List of (block idx, tx idx, TransactionReceipt)
+ *
+ * Since a transaction could be included into blocks from different forks and
+ * have different receipts the class stores all of them (the same manner fork blocks are stored)
  *
  * Created by Anton Nashatyrev on 07.04.2016.
  */
 @Component
-public class TransactionStore extends ObjectDataSource<TransactionInfo> {
-    private final static Serializer<TransactionInfo, byte[]> serializer =
-            new Serializer<TransactionInfo, byte[]>() {
+public class TransactionStore extends ObjectDataSource<List<TransactionInfo>> {
+    private final static Serializer<List<TransactionInfo>, byte[]> serializer =
+            new Serializer<List<TransactionInfo>, byte[]>() {
         @Override
-        public byte[] serialize(TransactionInfo object) {
-            return object.getEncoded();
+        public byte[] serialize(List<TransactionInfo> object) {
+            byte[][] txsRlp = new byte[object.size()][];
+            for (int i = 0; i < txsRlp.length; i++) {
+                txsRlp[i] = object.get(i).getEncoded();
+            }
+            return RLP.encodeList(txsRlp);
         }
 
         @Override
-        public TransactionInfo deserialize(byte[] stream) {
-            return new TransactionInfo(stream);
+        public List<TransactionInfo> deserialize(byte[] stream) {
+            try {
+                RLPList params = RLP.decode2(stream);
+                RLPList infoList = (RLPList) params.get(0);
+                List<TransactionInfo> ret = new ArrayList<>();
+                for (int i = 0; i < infoList.size(); i++) {
+                    ret.add(new TransactionInfo(infoList.get(i).getRLPData()));
+                }
+                return ret;
+            } catch (Exception e) {
+                // fallback to previous DB version
+                return Collections.singletonList(new TransactionInfo(stream));
+            }
         }
     };
+
+    /**
+     * Adds TransactionInfo to the store.
+     * If entries for this transaction already exist the method adds new entry to the list
+     * if no entry for the same block exists
+     * @return true if TransactionInfo was added, false if already exist
+     */
+    public boolean put(TransactionInfo tx) {
+        byte[] txHash = tx.getReceipt().getTransaction().getHash();
+        List<TransactionInfo> existingInfos = get(txHash);
+        if (existingInfos == null) {
+            existingInfos = new ArrayList<>();
+        } else {
+            for (TransactionInfo info : existingInfos) {
+                if (FastByteComparisons.equal(info.getBlockHash(), tx.getBlockHash())) {
+                    return false;
+                }
+            }
+        }
+        existingInfos.add(tx);
+        put(txHash, existingInfos);
+
+        return true;
+    }
 
     public TransactionStore(KeyValueDataSource src) {
         super(src, serializer);
