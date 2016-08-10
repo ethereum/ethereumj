090f16d8f4d0610d7a1e7f8a31053fbe67eb4255
Anton Nahsatyrev <anton.nashatyrev@gmail.com>
2016-08-10 20:19:01 +0300

    Add warning output if a task on EDT is executed for more than 1 second

--- ethereumj-core/src/main/java/org/ethereum/core/EventDispatchThread.java
+++ ethereumj-core/src/main/java/org/ethereum/core/EventDispatchThread.java
@@ -4,9 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * The class intended to serve as an 'Event Bus' where all EthereumJ events are
@@ -22,7 +20,14 @@ import java.util.concurrent.TimeUnit;
 public class EventDispatchThread {
     private static final Logger logger = LoggerFactory.getLogger("blockchain");
 
-    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private final BlockingQueue<Runnable> executorQueue = new LinkedBlockingQueue<Runnable>();
+    private final ExecutorService executor = new ThreadPoolExecutor(1, 1,
+            0L, TimeUnit.MILLISECONDS, executorQueue, new ThreadFactory() {
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "EDT");
+        }
+    });
 
     private static EventDispatchThread eventDispatchThread;
 
@@ -48,7 +53,14 @@ public class EventDispatchThread {
             @Override
             public void run() {
                 try {
+                    long s = System.nanoTime();
                     r.run();
+                    long t = (System.nanoTime() - s) / 1_000_000;
+                    if (t > 1000) {
+                        logger.warn("EDT task executed in more than 1 sec: " + t + " ms, " + this +
+                        ". Executor queue size: " + executorQueue.size());
+
+                    }
                 } catch (Exception e) {
                     logger.error("EDT task exception", e);
                 }
