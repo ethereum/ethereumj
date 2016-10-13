41f767ea143b56c3b4c869ec545c3b9289cd2abf
Eric Winer <enwiner@gmail.com>
2016-10-13 15:23:38 -0400

    cache the cache misses in DetailsDataStore

--- ethereumj-core/src/main/java/org/ethereum/db/DetailsDataStore.java
+++ ethereumj-core/src/main/java/org/ethereum/db/DetailsDataStore.java
@@ -72,7 +72,10 @@ public class DetailsDataStore {
 
             if (removes.contains(wrappedKey)) return null;
             byte[] data = detailsDS.get(key);
-            if (data == null) return null;
+            if (data == null) {
+                removes.add(wrappedKey); // keeps it from being looked up again next time
+                return null;
+            }
 
             ContractDetailsImpl detailsImpl = commonConfig.contractDetailsImpl();
             detailsImpl.setDataSource(storageDSPrune);
