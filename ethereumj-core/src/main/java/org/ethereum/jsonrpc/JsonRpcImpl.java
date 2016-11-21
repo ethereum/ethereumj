7f5af37f5decccdbc53f541c704e1780747f4ced
Stan Reshetnyk <stan@ether.camp>
2016-11-21 13:52:02 +0200

    Improve `eth_call` with support of `pending` state

--- ethereumj-core/src/main/java/org/ethereum/jsonrpc/JsonRpcImpl.java
+++ ethereumj-core/src/main/java/org/ethereum/jsonrpc/JsonRpcImpl.java
@@ -1,10 +1,12 @@
 package org.ethereum.jsonrpc;
 
 import org.apache.commons.collections4.map.LRUMap;
+import org.ethereum.config.CommonConfig;
 import org.ethereum.config.SystemProperties;
 import org.ethereum.core.*;
 import org.ethereum.crypto.ECKey;
 import org.ethereum.crypto.HashUtil;
+import org.ethereum.db.BlockStore;
 import org.ethereum.db.ByteArrayWrapper;
 import org.ethereum.core.TransactionInfo;
 import org.ethereum.db.TransactionStore;
@@ -25,6 +27,7 @@ import org.ethereum.util.ByteUtil;
 import org.ethereum.util.RLP;
 import org.ethereum.vm.DataWord;
 import org.ethereum.vm.LogInfo;
+import org.ethereum.vm.program.invoke.ProgramInvokeFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -125,6 +128,12 @@ public class JsonRpcImpl implements JsonRpc {
 
     @Autowired
     SolidityCompiler solidityCompiler;
+
+    @Autowired
+    ProgramInvokeFactory programInvokeFactory;
+
+    @Autowired
+    CommonConfig commonConfig = CommonConfig.getDefault();
 
     BlockchainImpl blockchain;
 
@@ -575,6 +584,14 @@ public class JsonRpcImpl implements JsonRpc {
     }
 
     public TransactionReceipt createCallTxAndExecute(CallArguments args, Block block) throws Exception {
+        Repository repository = ((Repository) worldManager.getRepository())
+                .getSnapshotTo(block.getStateRoot())
+                .startTracking();
+
+        return createCallTxAndExecute(args, block, repository, worldManager.getBlockStore());
+    }
+
+    public TransactionReceipt createCallTxAndExecute(CallArguments args, Block block, Repository repository, BlockStore blockStore) throws Exception {
         BinaryCallArguments bca = new BinaryCallArguments();
         bca.setArguments(args);
         Transaction tx = CallTransaction.createRawTransaction(0,
@@ -584,14 +601,39 @@ public class JsonRpcImpl implements JsonRpc {
                 bca.value,
                 bca.data);
 
-        return eth.callConstant(tx, block);
+        // put mock signature if not present
+        if (tx.getSignature() == null) {
+            tx.sign(ECKey.fromPrivate(new byte[32]));
+        }
+
+        try {
+            TransactionExecutor executor = commonConfig.transactionExecutor
+                    (tx, block.getCoinbase(), repository, blockStore,
+                            programInvokeFactory, block, new EthereumListenerAdapter(), 0)
+                    .setLocalCall(true);
+
+            executor.init();
+            executor.execute();
+            executor.go();
+            executor.finalization();
+
+            return executor.getReceipt();
+        } finally {
+            repository.rollback();
+        }
     }
 
     public String eth_call(CallArguments args, String bnOrId) throws Exception {
 
         String s = null;
         try {
-            TransactionReceipt res = createCallTxAndExecute(args, getByJsonBlockId(bnOrId));
+            TransactionReceipt res;
+            if ("pending".equals(bnOrId)) {
+                Block pendingBlock = blockchain.createNewBlock(blockchain.getBestBlock(), pendingState.getPendingTransactions(), Collections.<BlockHeader>emptyList());
+                res = createCallTxAndExecute(args, pendingBlock, pendingState.getRepository(), worldManager.getBlockStore());
+            } else {
+                res = createCallTxAndExecute(args, getByJsonBlockId(bnOrId));
+            }
             return s = TypeConverter.toJsonHex(res.getExecutionResult());
         } finally {
             if (logger.isDebugEnabled()) logger.debug("eth_call(" + args + "): " + s);
