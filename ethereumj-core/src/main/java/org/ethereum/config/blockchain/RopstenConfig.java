6244e013680682fc2f63f470bacace6c63a66c8f
Dmitry Shmatko <zilm@zilm.net>
2016-12-02 21:19:06 +0400

    Added Ropsten constraints block checking to drop fake peers

--- ethereumj-core/src/main/java/org/ethereum/config/blockchain/RopstenConfig.java
+++ ethereumj-core/src/main/java/org/ethereum/config/blockchain/RopstenConfig.java
@@ -1,17 +1,28 @@
 package org.ethereum.config.blockchain;
 
-import org.ethereum.config.Constants;
+import org.apache.commons.lang3.tuple.Pair;
+import org.ethereum.config.BlockchainConfig;
+import org.spongycastle.util.encoders.Hex;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by Anton Nashatyrev on 21.11.2016.
  */
-public class RopstenConfig extends HomesteadConfig {
+public class RopstenConfig extends Eip160HFConfig {
 
-    public RopstenConfig() {
+    // Check for 1 known block to exclude fake peers
+    private static final long CHECK_BLOCK_NUMBER = 10;
+    private static final byte[] CHECK_BLOCK_HASH = Hex.decode("b3074f936815a0425e674890d7db7b5e94f3a06dca5b22d291b55dcd02dde93e");
+
+    public RopstenConfig(BlockchainConfig parent) {
+        super(parent);
     }
 
-    public RopstenConfig(Constants constants) {
-        super(constants);
+    @Override
+    public List<Pair<Long, byte[]>> blockHashConstraints() {
+        return Collections.singletonList(Pair.of(CHECK_BLOCK_NUMBER, CHECK_BLOCK_HASH));
     }
 
     @Override
