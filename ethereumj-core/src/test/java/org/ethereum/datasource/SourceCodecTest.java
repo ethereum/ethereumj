532c52f942f7049fd21f9a0a12c4faea09b9903d
Dmitry Shmatko <zilm@zilm.net>
2016-12-08 23:55:07 +0400

    Added tests for Sources working with serializers

---
+++ ethereumj-core/src/test/java/org/ethereum/datasource/SourceCodecTest.java
@@ -0,0 +1,91 @@
+package org.ethereum.datasource;
+
+import org.ethereum.datasource.inmem.HashMapDB;
+import org.ethereum.vm.DataWord;
+import org.junit.Test;
+import org.spongycastle.util.encoders.Hex;
+
+import static org.ethereum.crypto.HashUtil.sha3;
+import static org.ethereum.util.ByteUtil.longToBytes;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Test for {@link SourceCodec}
+ */
+public class SourceCodecTest {
+
+    private byte[] intToKey(int i) {
+        return sha3(longToBytes(i));
+    }
+
+    private byte[] intToValue(int i) {
+        return (new DataWord(i)).getData();
+    }
+
+    private DataWord intToDataWord(int i) {
+        return new DataWord(i);
+    }
+
+    private DataWord intToDataWordKey(int i) {
+        return new DataWord(intToKey(i));
+    }
+
+    private String str(Object obj) {
+        if (obj == null) return null;
+
+        byte[] data;
+        if (obj instanceof DataWord) {
+            data = ((DataWord) obj).getData();
+        } else {
+            data = (byte[]) obj;
+        }
+
+        return Hex.toHexString(data);
+    }
+
+    @Test
+    public void testDataWordKeySerializer() {
+        Source<byte[], byte[]> parentSrc = new HashMapDB<>();
+        Serializer<DataWord, byte[]> keySerializer = Serializers.StorageKeySerializer;
+        Serializer<byte[], byte[]> valueSerializer = new Serializers.Identity<>();
+        SourceCodec<DataWord, byte[], byte[], byte[]> src = new SourceCodec<>(parentSrc, keySerializer, valueSerializer);
+
+        for (int i = 0; i < 10_000; ++i) {
+            src.put(intToDataWordKey(i), intToValue(i));
+        }
+
+        // Everything is in src
+        assertEquals(str(intToValue(0)), str(src.get(intToDataWordKey(0))));
+        assertEquals(str(intToValue(9_999)), str(src.get(intToDataWordKey(9_999))));
+
+        // Modifying key
+        src.put(intToDataWordKey(0), intToValue(12345));
+        assertEquals(str(intToValue(12345)), str(src.get(intToDataWordKey(0))));
+
+        // Testing there is no cache
+        assertEquals(str(intToValue(9_990)), str(src.get(intToDataWordKey(9_990))));
+        parentSrc.delete(keySerializer.serialize(intToDataWordKey(9_990)));
+        assertNull(src.get(intToDataWordKey(9_990)));
+    }
+
+    @Test
+    public void testDataWordKeyValueSerializer() {
+        Source<byte[], byte[]> parentSrc = new HashMapDB<>();
+        Serializer<DataWord, byte[]> keySerializer = Serializers.StorageKeySerializer;
+        Serializer<DataWord, byte[]> valueSerializer = Serializers.StorageValueSerializer;
+        SourceCodec<DataWord, DataWord, byte[], byte[]> src = new SourceCodec<>(parentSrc, keySerializer, valueSerializer);
+
+        for (int i = 0; i < 10_000; ++i) {
+            src.put(intToDataWordKey(i), intToDataWord(i));
+        }
+
+        // Everything is in src
+        assertEquals(str(intToDataWord(0)), str(src.get(intToDataWordKey(0))));
+        assertEquals(str(intToDataWord(9_999)), str(src.get(intToDataWordKey(9_999))));
+
+        // Modifying key
+        src.put(intToDataWordKey(0), intToDataWord(12345));
+        assertEquals(str(intToDataWord(12345)), str(src.get(intToDataWordKey(0))));
+    }
+}
