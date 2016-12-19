60b783e6d26406012ea3e38b7964701db65049f0
Dmitry Shmatko <zilm@zilm.net>
2016-12-19 23:49:14 +0400

    Fixed logging

--- ethereumj-core/src/test/java/org/ethereum/longrun/FastSyncSanityTest.java
+++ ethereumj-core/src/test/java/org/ethereum/longrun/FastSyncSanityTest.java
@@ -103,7 +103,6 @@ public class FastSyncSanityTest {
      */
     static class RegularNode extends BasicNode {
         public RegularNode() {
-            // peers need different loggers
             super("sampleNode");
         }
 
@@ -116,7 +115,7 @@ public class FastSyncSanityTest {
 
         @Override
         public void waitForSync() throws Exception {
-            logger.info("Waiting for the whole blockchain sync (will take up to an hour on fast sync for the whole chain)...");
+            testLogger.info("Waiting for the whole blockchain sync (will take up to an hour on fast sync for the whole chain)...");
             while(true) {
                 sleep(10000);
                 if (syncState == null) continue;
@@ -124,7 +123,7 @@ public class FastSyncSanityTest {
                 switch (syncState) {
                     case UNSECURE:
                         if (!firstRun.get()) break;
-                        logger.info("[v] Unsecure sync completed");
+                        testLogger.info("[v] Unsecure sync completed");
                         sleep(60000);
                         stopSync();
                         BlockchainValidation.checkNodes(ethereum, commonConfig, fatalErrors);
@@ -132,14 +131,14 @@ public class FastSyncSanityTest {
                         break;
                     case SECURE:
                         if (!secondRun.get()) break;
-                        logger.info("[v] Secure sync completed");
+                        testLogger.info("[v] Secure sync completed");
                         sleep(60000);
                         stopSync();
                         BlockchainValidation.checkFastHeaders(ethereum, commonConfig, fatalErrors);
                         secondRun.set(false);
                         break;
                     case COMPLETE:
-                        logger.info("[v] Sync complete! The best block: " + bestBlock.getShortDescr());
+                        testLogger.info("[v] Sync complete! The best block: " + bestBlock.getShortDescr());
                         stopSync();
                         return;
                 }
--- ethereumj-core/src/test/java/org/ethereum/longrun/SyncSanityTest.java
+++ ethereumj-core/src/test/java/org/ethereum/longrun/SyncSanityTest.java
@@ -101,18 +101,17 @@ public class SyncSanityTest {
      */
     static class RegularNode extends BasicNode {
         public RegularNode() {
-            // peers need different loggers
             super("sampleNode");
         }
 
         @Override
         public void waitForSync() throws Exception {
-            logger.info("Waiting for the whole blockchain sync (will take up to an hour on fast sync for the whole chain)...");
+            testLogger.info("Waiting for the whole blockchain sync (will take up to an hour on fast sync for the whole chain)...");
             while(true) {
                 sleep(10000);
 
                 if (syncComplete) {
-                    logger.info("[v] Sync complete! The best block: " + bestBlock.getShortDescr());
+                    testLogger.info("[v] Sync complete! The best block: " + bestBlock.getShortDescr());
 
                     // Stop syncing
                     config.setSyncEnabled(false);
