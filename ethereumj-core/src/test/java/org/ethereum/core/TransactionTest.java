96aadfaa8ca8e6df85558666c351977cdc4a6c32
Anton Nahsatyrev <anton.nashatyrev@gmail.com>
2016-12-20 23:38:29 +0300

    Fix test to submit 0 ethers with function call

--- ethereumj-core/src/test/java/org/ethereum/core/TransactionTest.java
+++ ethereumj-core/src/test/java/org/ethereum/core/TransactionTest.java
@@ -656,14 +656,14 @@ public class TransactionTest {
     }
 
     protected Transaction createTx(BlockchainImpl blockchain, ECKey sender, byte[] receiveAddress, byte[] data) {
-        return createTx(blockchain, sender, receiveAddress, data, 1);
+        return createTx(blockchain, sender, receiveAddress, data, 0);
     }
     protected Transaction createTx(BlockchainImpl blockchain, ECKey sender, byte[] receiveAddress,
                                    byte[] data, long value) {
         BigInteger nonce = blockchain.getRepository().getNonce(sender.getAddress());
         Transaction tx = new Transaction(
                 ByteUtil.bigIntegerToBytes(nonce),
-                ByteUtil.longToBytesNoLeadZeroes(1),
+                ByteUtil.longToBytesNoLeadZeroes(0),
                 ByteUtil.longToBytesNoLeadZeroes(3_000_000),
                 receiveAddress,
                 ByteUtil.longToBytesNoLeadZeroes(value),
