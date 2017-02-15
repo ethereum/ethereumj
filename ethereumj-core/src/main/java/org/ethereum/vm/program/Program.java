1182eb5ff217354f564a857f61d47ee867e73303
Anton Nahsatyrev <anton.nashatyrev@gmail.com>
2017-02-15 18:04:02 +0300

    Fix the case when cached Block hash byte[] is changed within VM DataWord

--- ethereumj-core/src/main/java/org/ethereum/vm/program/Program.java
+++ ethereumj-core/src/main/java/org/ethereum/vm/program/Program.java
@@ -677,7 +677,7 @@ public class Program {
 
     public DataWord getBlockHash(int index) {
         return index < this.getNumber().longValue() && index >= Math.max(256, this.getNumber().intValue()) - 256 ?
-                new DataWord(this.invoke.getBlockStore().getBlockHashByNumber(index, getPrevHash().getData())) :
+                new DataWord(this.invoke.getBlockStore().getBlockHashByNumber(index, getPrevHash().getData())).clone() :
                 DataWord.ZERO.clone();
     }
 
