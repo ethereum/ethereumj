544acfc44525f9adda1a78d8c7e235c82ffed3d6
Stan Reshetnyk <stan@ether.camp>
2017-02-27 19:46:01 +0200

    Support EIP 158 in genesis configs

--- ethereumj-core/src/main/java/org/ethereum/config/net/JsonNetConfig.java
+++ ethereumj-core/src/main/java/org/ethereum/config/net/JsonNetConfig.java
@@ -12,9 +12,21 @@ import java.util.*;
 public class JsonNetConfig extends BaseNetConfig {
 
     public JsonNetConfig(Map<String, String> config) throws RuntimeException {
-        final List<String> keys = Arrays.asList("homesteadBlock", "daoForkBlock", "EIP150Block", "EIP155Block", "EIP158Block");
+        final String EIP_155_BLOCK = "EIP155Block";
+        final String EIP_158_BLOCK = "EIP158Block";
+        final List<String> keys = Arrays.asList("homesteadBlock", "daoForkBlock", "EIP150Block", EIP_155_BLOCK, EIP_158_BLOCK);
         final ArrayList<Integer> orderedBlocks = new ArrayList<>();
         final HashMap<Integer, String> blockToKey = new HashMap<>();
+
+        // preparation
+        final String eip155Block = config.remove(EIP_155_BLOCK);
+        if (eip155Block != null) {
+            if (config.containsKey(EIP_158_BLOCK) && eip155Block.equalsIgnoreCase(config.get(EIP_158_BLOCK))) {
+                // all fine, we can handle this
+            } else {
+                LoggerFactory.getLogger("general").warn("Ignored config option EIP155Block: {}", eip155Block);
+            }
+        }
 
         // #1 create block number to config map
         for (String key : keys) {
@@ -58,12 +70,23 @@ public class JsonNetConfig extends BaseNetConfig {
                     }
                     add(blockNumber, new Eip150HFConfig(getConfigForBlock(prevBlockNumber)));
                     break;
+                case EIP_158_BLOCK:
+                    if (blockNumber == 0) {
+                        throw new RuntimeException("Unexpected and untested blockchain configuration");
+                    }
+                    add(blockNumber, new Eip160HFConfig(getConfigForBlock(prevBlockNumber)));
+                    break;
                 // TODO handle other EIP configs
                 default:
                     LoggerFactory.getLogger("general").warn("Ignored config option from genesis {}: {}", blockNumber, key);
                     continue;
             }
             prevBlockNumber = blockNumber;
+        }
+
+        // Check for chainId
+        if (config.containsKey("chainId")) {
+
         }
     }
 
