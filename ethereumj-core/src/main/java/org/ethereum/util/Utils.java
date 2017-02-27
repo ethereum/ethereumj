2d4d6a1a823cdfb72922094990817de7eff51882
Anton Nahsatyrev <anton.nashatyrev@gmail.com>
2017-02-27 22:52:56 +0300

    Add wire traffic stat monitor

--- ethereumj-core/src/main/java/org/ethereum/util/Utils.java
+++ ethereumj-core/src/main/java/org/ethereum/util/Utils.java
@@ -226,6 +226,13 @@ public class Utils {
         throw new RuntimeException(message);
     }
 
+    public static String sizeToStr(long size) {
+        if (size < 2 * (1L << 10)) return size + "b";
+        if (size < 2 * (1L << 20)) return String.format("%dKb", size / (1L << 10));
+        if (size < 2 * (1L << 30)) return String.format("%dMb", size / (1L << 20));
+        return String.format("%dGb", size / (1L << 30));
+    }
+
     public static void sleep(long ms) {
         try {
             Thread.sleep(ms);
