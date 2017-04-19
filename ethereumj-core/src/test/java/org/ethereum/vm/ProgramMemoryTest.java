593706d2eaee40cd23ba82709cd331fc0f40e35d
Arsalan Khalid <arsalankhalid@outlook.com>
2017-04-19 00:53:05 -0400

    Optimized test

--- ethereumj-core/src/test/java/org/ethereum/vm/ProgramMemoryTest.java
+++ ethereumj-core/src/test/java/org/ethereum/vm/ProgramMemoryTest.java
@@ -341,15 +341,11 @@ public class ProgramMemoryTest {
         assertEquals(32, program.getMemSize());
     }
 
-    @Ignore
     @Test
     public void testInitialInsert() {
-
-
-        // todo: fix the array out of bound here
         int offset = 32;
-        int size = 00;
-        program.memorySave(32, 0, new byte[0]);
+        int size = 0;
+        program.memorySave(offset,size, new byte[0]);
         assertEquals(32, program.getMemSize());
     }
 }
