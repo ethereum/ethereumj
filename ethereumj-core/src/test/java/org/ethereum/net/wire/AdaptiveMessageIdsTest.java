a4fff39c1ba94273ebc439d6533682d17e27c852
Martin Treurnicht <martin@gyft.com>
2017-10-11 16:59:11 -0700

    Added tests for checking and verifying capability offsets

--- ethereumj-core/src/test/java/org/ethereum/net/wire/AdaptiveMessageIdsTest.java
+++ ethereumj-core/src/test/java/org/ethereum/net/wire/AdaptiveMessageIdsTest.java
@@ -17,6 +17,13 @@
  */
 package org.ethereum.net.wire;
 
+import static org.ethereum.net.eth.EthVersion.V62;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.ethereum.net.client.Capability;
 import org.ethereum.net.eth.EthVersion;
 import org.ethereum.net.eth.message.EthMessageCodes;
@@ -24,16 +31,10 @@ import org.ethereum.net.p2p.P2pMessageCodes;
 import org.ethereum.net.rlpx.MessageCodesResolver;
 import org.ethereum.net.shh.ShhHandler;
 import org.ethereum.net.shh.ShhMessageCodes;
-
+import org.ethereum.net.swarm.bzz.BzzHandler;
+import org.ethereum.net.swarm.bzz.BzzMessageCodes;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-
-import static org.ethereum.net.eth.EthVersion.*;
 
 /**
  * @author Roman Mandeleil
@@ -113,40 +114,133 @@ public class AdaptiveMessageIdsTest {
 
         messageCodesResolver.init(capabilities);
 
-        assertEquals(0x10 + 0, messageCodesResolver.withEthOffset(EthMessageCodes.STATUS.asByte()));
-        assertEquals(0x10 + 1, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK_HASHES.asByte()));
-        assertEquals(0x10 + 2, messageCodesResolver.withEthOffset(EthMessageCodes.TRANSACTIONS.asByte()));
-        assertEquals(0x10 + 3, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_HEADERS.asByte()));
-        assertEquals(0x10 + 4, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_HEADERS.asByte()));
-        assertEquals(0x10 + 5, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_BODIES.asByte()));
-        assertEquals(0x10 + 6, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_BODIES.asByte()));
-        assertEquals(0x10 + 7, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK.asByte()));
+        int ethOffset = P2pMessageCodes.USER.asByte() + 1;
+        int shhOffset = ethOffset + EthMessageCodes.NEW_BLOCK.asByte() + 1;
 
-        assertEquals(0x18 + 0, messageCodesResolver.withShhOffset(ShhMessageCodes.STATUS.asByte()));
-        assertEquals(0x18 + 1, messageCodesResolver.withShhOffset(ShhMessageCodes.MESSAGE.asByte()));
-        assertEquals(0x18 + 2, messageCodesResolver.withShhOffset(ShhMessageCodes.FILTER.asByte()));
+        assertEquals(ethOffset + 0, messageCodesResolver.withEthOffset(EthMessageCodes.STATUS.asByte()));
+        assertEquals(ethOffset + 1, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK_HASHES.asByte()));
+        assertEquals(ethOffset + 2, messageCodesResolver.withEthOffset(EthMessageCodes.TRANSACTIONS.asByte()));
+        assertEquals(ethOffset + 3, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_HEADERS.asByte()));
+        assertEquals(ethOffset + 4, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_HEADERS.asByte()));
+        assertEquals(ethOffset + 5, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_BODIES.asByte()));
+        assertEquals(ethOffset + 6, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_BODIES.asByte()));
+        assertEquals(ethOffset + 7, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK.asByte()));
+
+        assertEquals(shhOffset + 0, messageCodesResolver.withShhOffset(ShhMessageCodes.STATUS.asByte()));
+        assertEquals(shhOffset + 1, messageCodesResolver.withShhOffset(ShhMessageCodes.MESSAGE.asByte()));
+        assertEquals(shhOffset + 2, messageCodesResolver.withShhOffset(ShhMessageCodes.FILTER.asByte()));
     }
 
     @Test // Capabilities should be read in alphabetical order
     public void test5() {
 
+        EthVersion ethVersion = EthVersion.V62;
         List<Capability> capabilities = Arrays.asList(
                 new Capability(Capability.SHH, ShhHandler.VERSION),
-                new Capability(Capability.ETH, EthVersion.V62.getCode()));
+                new Capability(Capability.ETH, ethVersion.getCode()));
 
         messageCodesResolver.init(capabilities);
 
-        assertEquals(0x10 + 0, messageCodesResolver.withEthOffset(EthMessageCodes.STATUS.asByte()));
-        assertEquals(0x10 + 1, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK_HASHES.asByte()));
-        assertEquals(0x10 + 2, messageCodesResolver.withEthOffset(EthMessageCodes.TRANSACTIONS.asByte()));
-        assertEquals(0x10 + 3, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_HEADERS.asByte()));
-        assertEquals(0x10 + 4, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_HEADERS.asByte()));
-        assertEquals(0x10 + 5, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_BODIES.asByte()));
-        assertEquals(0x10 + 6, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_BODIES.asByte()));
-        assertEquals(0x10 + 7, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK.asByte()));
+        int ethOffset = P2pMessageCodes.USER.asByte() + 1;
+        int shhOffset = ethOffset + EthMessageCodes.NEW_BLOCK.asByte() + 1;
 
-        assertEquals(0x18 + 0, messageCodesResolver.withShhOffset(ShhMessageCodes.STATUS.asByte()));
-        assertEquals(0x18 + 1, messageCodesResolver.withShhOffset(ShhMessageCodes.MESSAGE.asByte()));
-        assertEquals(0x18 + 2, messageCodesResolver.withShhOffset(ShhMessageCodes.FILTER.asByte()));
+        assertEquals(ethOffset + 0, messageCodesResolver.withEthOffset(EthMessageCodes.STATUS.asByte()));
+        assertEquals(ethOffset + 1, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK_HASHES.asByte()));
+        assertEquals(ethOffset + 2, messageCodesResolver.withEthOffset(EthMessageCodes.TRANSACTIONS.asByte()));
+        assertEquals(ethOffset + 3, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_HEADERS.asByte()));
+        assertEquals(ethOffset + 4, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_HEADERS.asByte()));
+        assertEquals(ethOffset + 5, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_BODIES.asByte()));
+        assertEquals(ethOffset + 6, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_BODIES.asByte()));
+        assertEquals(ethOffset + 7, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK.asByte()));
+
+        assertFalse(EthMessageCodes.inRange((byte)(shhOffset + 0), ethVersion));
+        assertEquals(shhOffset + 0, messageCodesResolver.withShhOffset(ShhMessageCodes.STATUS.asByte()));
+        assertEquals(shhOffset + 1, messageCodesResolver.withShhOffset(ShhMessageCodes.MESSAGE.asByte()));
+        assertEquals(shhOffset + 2, messageCodesResolver.withShhOffset(ShhMessageCodes.FILTER.asByte()));
+    }
+
+    @Test // Verify that eth63 offset is working correctly
+    public void test6() {
+
+        EthVersion ethVersion = EthVersion.V63;
+        List<Capability> capabilities = Arrays.asList(
+                new Capability(Capability.SHH, ShhHandler.VERSION),
+                new Capability(Capability.ETH, ethVersion.getCode()));
+
+        messageCodesResolver.init(capabilities);
+
+        int ethOffset = P2pMessageCodes.USER.asByte() + 1;
+        int shhOffset = ethOffset + EthMessageCodes.RECEIPTS.asByte() + 1;
+        // Verify Eth Offset Lower Boundary
+        byte ethCode = messageCodesResolver.resolveEth(messageCodesResolver.withP2pOffset(P2pMessageCodes.USER.asByte()));
+        assertFalse(EthMessageCodes.inRange(ethCode, ethVersion));
+        assertEquals(ethOffset + 0, messageCodesResolver.withEthOffset(EthMessageCodes.STATUS.asByte()));
+        assertEquals(ethOffset + 1, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK_HASHES.asByte()));
+        assertEquals(ethOffset + 2, messageCodesResolver.withEthOffset(EthMessageCodes.TRANSACTIONS.asByte()));
+        assertEquals(ethOffset + 3, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_HEADERS.asByte()));
+        assertEquals(ethOffset + 4, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_HEADERS.asByte()));
+        assertEquals(ethOffset + 5, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_BODIES.asByte()));
+        assertEquals(ethOffset + 6, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_BODIES.asByte()));
+        assertEquals(ethOffset + 7, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK.asByte()));
+        assertEquals(ethOffset + 13, messageCodesResolver.withEthOffset(EthMessageCodes.GET_NODE_DATA.asByte()));
+        assertEquals(ethOffset + 14, messageCodesResolver.withEthOffset(EthMessageCodes.NODE_DATA.asByte()));
+        assertEquals(ethOffset + 15, messageCodesResolver.withEthOffset(EthMessageCodes.GET_RECEIPTS.asByte()));
+        assertEquals(ethOffset + 16, messageCodesResolver.withEthOffset(EthMessageCodes.RECEIPTS.asByte()));
+
+        // Verify Eth Upper boundary
+        ethCode = messageCodesResolver.resolveEth((byte)(shhOffset + 0));
+        assertFalse(EthMessageCodes.inRange(ethCode, ethVersion));
+
+        // Verify Shh Lower Boundary
+        byte shhCode = messageCodesResolver.resolveShh((byte)(ethOffset + 16));
+        assertFalse(ShhMessageCodes.inRange(shhCode));
+        assertEquals(shhOffset + 0, messageCodesResolver.withShhOffset(ShhMessageCodes.STATUS.asByte()));
+        assertEquals(shhOffset + 1, messageCodesResolver.withShhOffset(ShhMessageCodes.MESSAGE.asByte()));
+        assertEquals(shhOffset + 2, messageCodesResolver.withShhOffset(ShhMessageCodes.FILTER.asByte()));
+    }
+
+    @Test // Checks that offsets works correctly when all capabilities are activated
+    public void test7() {
+        EthVersion ethVersion = EthVersion.V63;
+        List<Capability> capabilities = Arrays.asList(
+                new Capability(Capability.ETH, ethVersion.getCode()),
+                new Capability(Capability.SHH, ShhHandler.VERSION),
+                new Capability(Capability.BZZ, BzzHandler.VERSION));
+
+        messageCodesResolver.init(capabilities);
+
+        int bzzOffset = P2pMessageCodes.USER.asByte() + 1;
+        int ethOffset = bzzOffset + BzzMessageCodes.PEERS.asByte() + 1;
+        int shhOffset = ethOffset + EthMessageCodes.RECEIPTS.asByte() + 1;
+        // Verify Bzz Offset Lower Boundary
+        byte bzzCode = messageCodesResolver.resolveBzz(messageCodesResolver.withP2pOffset(P2pMessageCodes.USER.asByte()));
+        assertFalse(BzzMessageCodes.inRange(bzzCode));
+        assertEquals(bzzOffset + 0, messageCodesResolver.withBzzOffset(BzzMessageCodes.STATUS.asByte()));
+        assertEquals(bzzOffset + 1, messageCodesResolver.withBzzOffset(BzzMessageCodes.STORE_REQUEST.asByte()));
+        assertEquals(bzzOffset + 2, messageCodesResolver.withBzzOffset(BzzMessageCodes.RETRIEVE_REQUEST.asByte()));
+        assertEquals(bzzOffset + 3, messageCodesResolver.withBzzOffset(BzzMessageCodes.PEERS.asByte()));
+        // Verify Bzz Offset Upper Boundary
+        bzzCode = messageCodesResolver.resolveBzz((byte) (ethOffset + 0));
+        assertFalse(BzzMessageCodes.inRange(bzzCode));
+        // Verify Eth Offset Lower Boundary
+        byte ethCode = messageCodesResolver.resolveEth((byte)(bzzOffset + 3));
+        assertFalse(EthMessageCodes.inRange(ethCode, ethVersion));
+        assertEquals(ethOffset + 0, messageCodesResolver.withEthOffset(EthMessageCodes.STATUS.asByte()));
+        assertEquals(ethOffset + 1, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK_HASHES.asByte()));
+        assertEquals(ethOffset + 2, messageCodesResolver.withEthOffset(EthMessageCodes.TRANSACTIONS.asByte()));
+        assertEquals(ethOffset + 3, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_HEADERS.asByte()));
+        assertEquals(ethOffset + 4, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_HEADERS.asByte()));
+        assertEquals(ethOffset + 5, messageCodesResolver.withEthOffset(EthMessageCodes.GET_BLOCK_BODIES.asByte()));
+        assertEquals(ethOffset + 6, messageCodesResolver.withEthOffset(EthMessageCodes.BLOCK_BODIES.asByte()));
+        assertEquals(ethOffset + 7, messageCodesResolver.withEthOffset(EthMessageCodes.NEW_BLOCK.asByte()));
+        // Verify Eth Offset Upper Boundary
+        ethCode = messageCodesResolver.resolveEth((byte)(shhOffset + 0));
+        assertFalse(EthMessageCodes.inRange(ethCode, ethVersion));
+        // Verify Shh Offset Lower Boundary
+        byte shhCode = messageCodesResolver.resolveShh((byte)(ethOffset + 7));
+        assertFalse(ShhMessageCodes.inRange(shhCode));
+        assertEquals(shhOffset + 0, messageCodesResolver.withShhOffset(ShhMessageCodes.STATUS.asByte()));
+        assertEquals(shhOffset + 1, messageCodesResolver.withShhOffset(ShhMessageCodes.MESSAGE.asByte()));
+        assertEquals(shhOffset + 2, messageCodesResolver.withShhOffset(ShhMessageCodes.FILTER.asByte()));
     }
 }
