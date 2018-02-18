e52e260f887176af52e5f54787a6d759741a5769
Nicosia <nicosia@kth.se>
2018-02-18 09:46:53 +0100

    Refactor [#62]: Rename StorageTest to DefaultStorageTest (#63)

--- ethereumj-core/src/test/java/org/ethereum/vm/program/DefaultStorageTest.java
+++ ethereumj-core/src/test/java/org/ethereum/vm/program/DefaultStorageTest.java
@@ -22,7 +22,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class StorageTest {
+public class DefaultStorageTest {
 
     @Rule
     public final ExpectedException exception = ExpectedException.none();
