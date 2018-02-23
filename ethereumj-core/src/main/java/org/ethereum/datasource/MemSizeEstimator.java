b2adf4bd1b568151a0f4ccf338358ff4688dbcc2
Mikhail Kalinin <kalinin128@gmail.com>
2018-02-23 20:09:24 +0600

    Tune ByteArrayEstimator

--- ethereumj-core/src/main/java/org/ethereum/datasource/MemSizeEstimator.java
+++ ethereumj-core/src/main/java/org/ethereum/datasource/MemSizeEstimator.java
@@ -30,6 +30,8 @@ public interface MemSizeEstimator<E> {
      * byte[] type size estimator
      */
     MemSizeEstimator<byte[]> ByteArrayEstimator = bytes -> {
-        return bytes == null ? 0 : bytes.length + 4; // 4 - compressed ref size
+        return bytes == null ? 0 : bytes.length + 16; // 4 - compressed ref size, 12 - Object header
     };
+
+
 }
