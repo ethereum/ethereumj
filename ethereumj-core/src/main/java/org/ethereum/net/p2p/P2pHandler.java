224e65b9b4ddcb46198a6f8faf69edc65d34d382
秦鹏飞 <fei_qihang@163.com>
2018-02-27 18:22:17 +0800

    Resolve ClassCastException
    When the "peer.p2p.pingInterval" parameter is configured in the ethereumj.conf file, the exception "java.lang.ClassCastException: java.lang.Integer can not be cast to java.lang.Long" appears. After analyzing the code, we found that in the getAnyRef method of the com.typesafe.config.Config class, the java.lang.Integer type is returned when getting the numeric parameter. And, I think the unit for the pingInterval parameter is seconds, using the Integer type is appropriate.

--- ethereumj-core/src/main/java/org/ethereum/net/p2p/P2pHandler.java
+++ ethereumj-core/src/main/java/org/ethereum/net/p2p/P2pHandler.java
@@ -281,7 +281,7 @@ public class P2pHandler extends SimpleChannelInboundHandler<P2pMessage> {
             } catch (Throwable t) {
                 logger.error("Unhandled exception", t);
             }
-        }, 2, config.getProperty("peer.p2p.pingInterval", 5L), TimeUnit.SECONDS);
+        }, 2, config.getProperty("peer.p2p.pingInterval", 5), TimeUnit.SECONDS);
     }
 
     public void killTimers() {
