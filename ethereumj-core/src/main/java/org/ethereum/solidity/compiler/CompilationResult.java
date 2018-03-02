6f295215db8b3bd6f879cfae7e19501c9f9f1af3
Thomas Glaeser <tglaeser@users.noreply.github.com>
2018-03-02 13:53:31 -0500

    Adding new methods to access the contract metadata

--- ethereumj-core/src/main/java/org/ethereum/solidity/compiler/CompilationResult.java
+++ ethereumj-core/src/main/java/org/ethereum/solidity/compiler/CompilationResult.java
@@ -17,20 +17,26 @@
  */
 package org.ethereum.solidity.compiler;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CompilationResult {
 
-    public Map<String, ContractMetadata> contracts;
-    public String version;
+    @JsonProperty("contracts") private Map<String, ContractMetadata> contracts;
+    @JsonProperty("version") public String version;
 
-    public static CompilationResult parse(String rawJson) throws IOException {
+    @JsonIgnore public static CompilationResult parse(String rawJson) throws IOException {
         if(rawJson == null || rawJson.isEmpty()){
             CompilationResult empty = new CompilationResult();
             empty.contracts = Collections.emptyMap();
@@ -40,6 +46,55 @@ public class CompilationResult {
         } else {
             return new ObjectMapper().readValue(rawJson, CompilationResult.class);
         }
+    }
+
+    @JsonIgnore public Path getContractPath() {
+        if (contracts.size() > 1) {
+            throw new UnsupportedOperationException("Source contains more than 1 contact. Please specify the contract name. Available keys (" + getContractKeys().values() + ").");
+        } else {
+            String key = contracts.keySet().iterator().next();
+            return Paths.get(key.substring(0, key.lastIndexOf(':')));
+        }
+    }
+
+    @JsonIgnore public String getContractName() {
+        if (contracts.size() > 1) {
+            throw new UnsupportedOperationException("Source contains more than 1 contact. Please specify the contract name. Available keys (" + getContractKeys().values() + ").");
+        } else {
+            String key = contracts.keySet().iterator().next();
+            return key.substring(key.lastIndexOf(':') + 1);
+        }
+    }
+
+    @JsonIgnore public Map<Path, String> getContractKeys() {
+        return contracts.keySet().stream().collect(Collectors.toMap(
+                key -> Paths.get(key.substring(0, key.lastIndexOf(':'))),
+                key -> key.substring(key.lastIndexOf(':') + 1)
+        ));
+    }
+
+    @JsonIgnore public ContractMetadata getContract(String contractName) {
+        if (contractName == null && contracts.size() == 1) {
+            return contracts.values().iterator().next();
+        } else if (contractName == null || contractName.isEmpty()) {
+            throw new UnsupportedOperationException("Source contains more than 1 contact. Please specify the contract name. Available keys (" + getContractKeys().values() + ").");
+        }
+        for (Map.Entry<String, ContractMetadata> entry : contracts.entrySet()) {
+            String key = entry.getKey();
+            String name = key.substring(key.lastIndexOf(':') + 1);
+            if (contractName.equals(name)) {
+                return entry.getValue();
+            }
+        }
+        throw new UnsupportedOperationException("Source contains more than 1 contact. Please specify a valid contract name. Available keys (" + getContractKeys().values() + ").");
+    }
+
+    @JsonIgnore public ContractMetadata getContract(Path contractPath, String contractName) {
+        return contracts.get(contractPath.toAbsolutePath().toString() + ':' + contractName);
+    }
+
+    @JsonIgnore public Collection<ContractMetadata> getContracts() {
+        return contracts.values();
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
