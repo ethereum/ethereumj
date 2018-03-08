e0ff85f618f469962fcc373c79a43f2a0f7c7e1d
Mikhail Kalinin <kalinin128@gmail.com>
2018-03-08 20:04:53 +0600

    Fix overflows in QuotientFilter constructor

--- ethereumj-core/src/main/java/org/ethereum/datasource/QuotientFilter.java
+++ ethereumj-core/src/main/java/org/ethereum/datasource/QuotientFilter.java
@@ -135,7 +135,7 @@ public class QuotientFilter implements Iterable<Long> {
     }
 
     static int TABLE_SIZE(int quotientBits, int remainderBits) {
-        long bits = (1 << quotientBits) * (remainderBits + 3);
+        long bits = (1L << quotientBits) * (remainderBits + 3);
         long longs = bits / 64;
         return Ints.checkedCast((bits % 64) > 0 ? (longs + 1) : longs);
     }
@@ -192,7 +192,7 @@ public class QuotientFilter implements Iterable<Long> {
         INDEX_MASK = LOW_MASK(QUOTIENT_BITS);
         REMAINDER_MASK = LOW_MASK(REMAINDER_BITS);
         ELEMENT_MASK = LOW_MASK(ELEMENT_BITS);
-        MAX_SIZE = 1 << QUOTIENT_BITS;
+        MAX_SIZE = 1L << QUOTIENT_BITS;
         MAX_INSERTIONS = (long) (MAX_SIZE * .75);
         table = new long[TABLE_SIZE(QUOTIENT_BITS, REMAINDER_BITS)];
         entries = 0;
