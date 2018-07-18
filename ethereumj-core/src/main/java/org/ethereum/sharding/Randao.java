4ddd54363c6256d2087c174e3fa17eb5fc505d6e
Mikhail Kalinin <kalinin128@gmail.com>
2018-07-18 17:49:29 +0600

    Add custom seed param to Randao.generate()

--- ethereumj-core/src/main/java/org/ethereum/sharding/Randao.java
+++ ethereumj-core/src/main/java/org/ethereum/sharding/Randao.java
@@ -1,17 +1,28 @@
 package org.ethereum.sharding;
 
-import org.ethereum.crypto.HashUtil;
 import org.ethereum.datasource.DbSource;
 
 import java.security.SecureRandom;
 
+import static org.ethereum.crypto.HashUtil.sha3;
+
 /**
+ * Generates and maintains data samples created in a hash onion fashion.
+ *
+ * <p>
+ *     That is a list of images made with a series of functions: <code>H_n(... H_2(H_1(s)))</code>.
+ *
+ * <p>
+ *     Use {@link #generate(int)} to create a list of images and then {@link #reveal()} to pick last image.
+ *
  * @author Mikhail Kalinin
  * @since 17.07.2018
  */
 public class Randao {
 
-    private static final byte[] NEXT_KEY = HashUtil.sha3("the_next_randao_image_key".getBytes());
+    private static final byte[] NEXT_KEY = sha3("the_next_randao_image_key".getBytes());
+    private static final int IMAGE_SIZE = 32;
+
     DbSource<byte[]> src;
     byte[] nextKey;
 
@@ -21,19 +32,29 @@ public class Randao {
     }
 
     public void generate(int rounds) {
+        byte[] seed = new byte[IMAGE_SIZE];
+        new SecureRandom().nextBytes(seed);
+        generate(rounds, seed);
+    }
+
+    public void generate(int rounds, byte[] seed) {
+        assert seed != null;
+
+        // update seed length to IMAGE_SIZE if needed
+        if (seed.length != IMAGE_SIZE) {
+            seed = sha3(seed);
+        }
+
         // reset datasource
         src.reset();
 
-        byte[] img = new byte[32];
-        new SecureRandom().nextBytes(img);
-
         for (int i = 0; i < rounds; i++) {
-            byte[] next = HashUtil.sha3(img);
-            src.put(next, img);
-            img = next;
+            byte[] next = sha3(seed);
+            src.put(next, seed);
+            seed = next;
         }
 
-        updateNextKey(img);
+        updateNextKey(seed);
     }
 
     public byte[] reveal() {
