58f09c17d763cf791a9213a3e857b13d65db0d57
e.shevchenko <eugene@ethergit.com>
2018-09-11 14:41:55 +0300

    Makes all methods of VMHook interface default to avoid creating adaptor.

--- ethereumj-core/src/main/java/org/ethereum/vm/VMHook.java
+++ ethereumj-core/src/main/java/org/ethereum/vm/VMHook.java
@@ -23,7 +23,15 @@ import org.ethereum.vm.program.Program;
  * Created by Anton Nashatyrev on 15.02.2016.
  */
 public interface VMHook {
-    void startPlay(Program program);
-    void step(Program program, OpCode opcode);
-    void stopPlay(Program program);
+    default void startPlay(Program program){
+
+    }
+
+    default void step(Program program, OpCode opcode) {
+
+    }
+
+    default void stopPlay(Program program) {
+
+    }
 }
