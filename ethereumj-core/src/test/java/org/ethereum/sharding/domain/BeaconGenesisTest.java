5b1a81a5ee65cc156d8c36c55b68e740a791d543
Mikhail Kalinin <kalinin128@gmail.com>
2018-09-14 19:47:01 +0600

    Add odd-length data to beacon-genesis-parse-test.json

--- ethereumj-core/src/test/java/org/ethereum/sharding/domain/BeaconGenesisTest.java
+++ ethereumj-core/src/test/java/org/ethereum/sharding/domain/BeaconGenesisTest.java
@@ -36,7 +36,7 @@ public class BeaconGenesisTest {
 
         assertArrayEquals(Hex.decode("d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3"), genesis.getParentHash());
         assertArrayEquals(Hex.decode("c85ef7d79691fe79573b1a7064c19c1a9819ebdbd1faaab1a8ec92344438aaf4"), genesis.getRandaoReveal());
-        assertArrayEquals(Hex.decode("41791102999c339c844880b23950704cc43aa840f3739e365323cda4dfa89e7a"), genesis.getMainChainRef());
+        assertArrayEquals(Hex.decode("01791102999c339c844880b23950704cc43aa840f3739e365323cda4dfa89e7a"), genesis.getMainChainRef());
         assertEquals(1535474832000L, genesis.getTimestamp());
     }
 
