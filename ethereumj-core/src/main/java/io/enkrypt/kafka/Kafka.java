71c0d0356ceb7372f98aa2a9c7ede14754ee846e
Brian <brian@bluerosedev.com>
2018-11-12 12:52:14 +0100

    Minor kafka adjustment

--- ethereumj-core/src/main/java/io/enkrypt/kafka/Kafka.java
+++ ethereumj-core/src/main/java/io/enkrypt/kafka/Kafka.java
@@ -6,7 +6,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 
 public interface Kafka {
 
-  String TOPIC_BLOCKS = "blocks";
+  String TOPIC_BLOCKS = "block-summaries";
   String TOPIC_PENDING_TRANSACTIONS = "pending-transactions";
 
   KafkaProducer<byte[], TransactionRecord> getPendingTransactionsProducer();
