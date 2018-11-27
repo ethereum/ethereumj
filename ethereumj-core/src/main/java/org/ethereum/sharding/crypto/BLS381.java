c3975b4a55e27e9c1080d6d25790dcb6193eabfb
Dmitry Shmatko <zilm@zilm.net>
2018-11-27 22:11:17 +0300

    Milagro bls381 implementation wrapped with Java-typed adapter

--- ethereumj-core/src/main/java/org/ethereum/sharding/crypto/BLS381.java
+++ ethereumj-core/src/main/java/org/ethereum/sharding/crypto/BLS381.java
@@ -3,70 +3,31 @@ package org.ethereum.sharding.crypto;
 import org.apache.milagro.amcl.BLS381.BIG;
 import org.apache.milagro.amcl.BLS381.ECP;
 import org.apache.milagro.amcl.BLS381.ECP2;
-import org.apache.milagro.amcl.BLS381.FP12;
-import org.apache.milagro.amcl.BLS381.PAIR;
-import org.apache.milagro.amcl.BLS381.ROM;
-import org.apache.milagro.amcl.RAND;
 
-import java.security.SecureRandom;
-
-import static org.apache.milagro.amcl.BLS381.BIG.MODBYTES;
+import static org.ethereum.sharding.crypto.BLS381Milagro.ECP2_POINT_SIZE;
+import static org.ethereum.sharding.crypto.BLS381Milagro.PRIVATE_SIZE;
 
 public class BLS381 {
 
-    public BIG CURVE_ORDER = new BIG(ROM.CURVE_Order);
-
-    public int PRIVATE_SIZE = MODBYTES;
-
-    public int ECP_POINT_SIZE = 2 * MODBYTES + 1;
-
-    public int ECP2_POINT_SIZE = 4 * MODBYTES;
-
-    private SecureRandom random = new SecureRandom();
-
-    /**
-     * Creates new random Signature (Private) key
-     */
-    public byte[] newSigKey() {
-        byte[] res = new byte[PRIVATE_SIZE];
-        RAND rand = new RAND();
-        byte[] randomBytes = new byte[PRIVATE_SIZE];
-        random.nextBytes(randomBytes);
-        rand.seed(PRIVATE_SIZE, randomBytes);
-        BIG randomNumber = BIG.randomnum(CURVE_ORDER, rand);
-        randomNumber.toBytes(res);
-
-        return res;
-    }
-
-    /**
-     * Obtains Verification (Public) key
-     * from Signature (Private) key on ECP2
-     */
-    public byte[] fromSigKey(BIG sigKey) {
-        ECP2 point = generator2();
-        return ecp2ToBytes(point.mul(sigKey));
-    }
-
-    private byte[] ecp2ToBytes(ECP2 point) {
-        byte[] res = new byte[ECP2_POINT_SIZE];
-        point.toBytes(res);
-
-        return res;
-    }
-
-    private ECP2 generator2() {
-        return ECP2.generator();
-    }
+    BLS381Milagro milagro = new BLS381Milagro();
 
     /**
      * Creates new random pair of Signature (Private) and
      * Verification (Public) keys
      */
     public KeyPair newKeyPair() {
+        BLS381Milagro.KeyPair keyPair = milagro.newKeyPair();
         KeyPair res = new KeyPair();
-        res.sigKey = newSigKey();
-        res.verKey = fromSigKey(BIG.fromBytes(res.sigKey));
+        res.sigKey = new byte[PRIVATE_SIZE];
+        keyPair.sigKey.toBytes(res.sigKey);
+        res.verKey = ecp2ToBytes(keyPair.verKey);
+
+        return res;
+    }
+
+    private byte[] ecp2ToBytes(ECP2 point) {
+        byte[] res = new byte[ECP2_POINT_SIZE];
+        point.toBytes(res);
 
         return res;
     }
@@ -78,18 +39,7 @@ public class BLS381 {
      * @return  signature, point on G1 (bytes)
      */
     public byte[] signMessage(byte[] sigKey, byte[] hash) {
-        // Map hash value to GroupG1 (ECP)
-        ECP point = ECP.mapit(hash);
-        ECP g1 = point.mul(BIG.fromBytes(sigKey));
-
-        return ecpToBytes(g1);
-    }
-
-    private byte[] ecpToBytes(ECP point) {
-        byte[] res = new byte[ECP_POINT_SIZE];
-        point.toBytes(res, false);
-
-        return res;
+        return milagro.signMessage(BIG.fromBytes(sigKey), hash);
     }
 
     /**
@@ -101,17 +51,8 @@ public class BLS381 {
      * @return  true if message is signature is done with the key
      */
     public boolean verifyMessage(byte[] sig, byte[] hash, byte[] verKey) {
-        ECP2 generator = generator2();
-        ECP point = ECP.mapit(hash);
-        FP12 lhs = atePairing(generator, ECP.fromBytes(sig));  // sig to ECP2
-        FP12 rhs = atePairing(ECP2.fromBytes(verKey), point); // verKey to ECP
-
-        return lhs.equals(rhs);
-    }
-
-    private FP12 atePairing(ECP2 point2, ECP point) {
-        FP12 p = PAIR.ate(point2, point);
-        return PAIR.fexp(p);
+        // sig to ECP2, verKey to ECP
+        return milagro.verifyMessage(ECP.fromBytes(sig), hash, ECP2.fromBytes(verKey));
     }
 
     class KeyPair {
---
+++ ethereumj-core/src/main/java/org/ethereum/sharding/crypto/BLS381Milagro.java
@@ -0,0 +1,115 @@
+package org.ethereum.sharding.crypto;
+
+import org.apache.milagro.amcl.BLS381.BIG;
+import org.apache.milagro.amcl.BLS381.ECP;
+import org.apache.milagro.amcl.BLS381.ECP2;
+import org.apache.milagro.amcl.BLS381.FP12;
+import org.apache.milagro.amcl.BLS381.PAIR;
+import org.apache.milagro.amcl.BLS381.ROM;
+import org.apache.milagro.amcl.RAND;
+
+import java.security.SecureRandom;
+
+import static org.apache.milagro.amcl.BLS381.BIG.MODBYTES;
+
+/**
+ * Implementation uses internal {@link org.apache.milagro.amcl} types
+ */
+public class BLS381Milagro {
+
+    public static BIG CURVE_ORDER = new BIG(ROM.CURVE_Order);
+
+    public static int PRIVATE_SIZE = MODBYTES;
+
+    public static int ECP_POINT_SIZE = 2 * MODBYTES + 1;
+
+    public static int ECP2_POINT_SIZE = 4 * MODBYTES;
+
+    private SecureRandom random = new SecureRandom();
+
+    /**
+     * Creates new random Signature (Private) key
+     */
+    public BIG newSigKey() {
+        RAND rand = new RAND();
+        byte[] randomBytes = new byte[PRIVATE_SIZE];
+        random.nextBytes(randomBytes);
+        rand.seed(PRIVATE_SIZE, randomBytes);
+        BIG randomNumber = BIG.randomnum(CURVE_ORDER, rand);
+
+        return randomNumber;
+    }
+
+    /**
+     * Obtains Verification (Public) key
+     * from Signature (Private) key on ECP2
+     */
+    public ECP2 fromSigKey(BIG sigKey) {
+        ECP2 point = generator2();
+        return point.mul(sigKey);
+    }
+
+    private ECP2 generator2() {
+        return ECP2.generator();
+    }
+
+    /**
+     * Creates new random pair of Signature (Private) and
+     * Verification (Public) keys
+     */
+    public KeyPair newKeyPair() {
+        KeyPair res = new KeyPair();
+        res.sigKey = newSigKey();
+        res.verKey = fromSigKey(res.sigKey);
+
+        return res;
+    }
+
+    /**
+     * Signs the message using its hash
+     * @param sigKey    Private key
+     * @param hash      Message hash, expects 384 bits
+     * @return  signature, point on G1 (bytes)
+     */
+    public byte[] signMessage(BIG sigKey, byte[] hash) {
+        // Map hash value to GroupG1 (ECP)
+        ECP point = ECP.mapit(hash);
+        ECP g1 = point.mul(sigKey);
+
+        return ecpToBytes(g1);
+    }
+
+    private byte[] ecpToBytes(ECP point) {
+        byte[] res = new byte[ECP_POINT_SIZE];
+        point.toBytes(res, false);
+
+        return res;
+    }
+
+    /**
+     * Verifies 384-bit hash and signature sig using Verification (Public)
+     * key verKey. Returns true if verification succeeded.
+     * @param sig       Signature, G1 point
+     * @param hash      message hash
+     * @param verKey    Verification key, G2 point
+     * @return  true if message is signature is done with the key
+     */
+    public boolean verifyMessage(ECP sig, byte[] hash, ECP2 verKey) {
+        ECP2 generator = generator2();
+        ECP point = ECP.mapit(hash);
+        FP12 lhs = atePairing(generator, sig);
+        FP12 rhs = atePairing(verKey, point);
+
+        return lhs.equals(rhs);
+    }
+
+    private FP12 atePairing(ECP2 point2, ECP point) {
+        FP12 p = PAIR.ate(point2, point);
+        return PAIR.fexp(p);
+    }
+
+    class KeyPair {
+        BIG sigKey;  // Signature (private key), point in GroupG1
+        ECP2 verKey;  // Verification (public key), point in GroupG2
+    }
+}
